dc79f678c36082f0efa98ed1f373c5a9a0590caf
RossAsscends <124905043+RossAscends@users.noreply.github.com>
2023-03-23 02:08:38 +0900

    moved location of some AI config toggles moved various API presets into the sliders tab  -so we can see what the presets are doing in real time.

--- public/script.js
+++ public/script.js
@@ -1819,6 +1819,7 @@ function changeMainAPI() {
         "kobold": {
             apiSettings: $("#kobold_api-settings"),
             apiConnector: $("#kobold_api"),
+            apiPresets: $('#kobold_api-presets'),
             apiRanges: $("#range_block"),
             maxContextElem: $("#max_context_block"),
             amountGenElem: $("#amount_gen_block"),
@@ -1827,6 +1828,7 @@ function changeMainAPI() {
         "textgenerationwebui": {
             apiSettings: $("#textgenerationwebui_api-settings"),
             apiConnector: $("#textgenerationwebui_api"),
+            apiPresets: $('#textgenerationwebui_api-presets'),
             apiRanges: $("#range_block_textgenerationwebui"),
             maxContextElem: $("#max_context_block"),
             amountGenElem: $("#amount_gen_block"),
@@ -1835,6 +1837,7 @@ function changeMainAPI() {
         "novel": {
             apiSettings: $("#novel_api-settings"),
             apiConnector: $("#novel_api"),
+            apiPresets: $('#novel_api-presets'),
             apiRanges: $("#range_block_novel"),
             maxContextElem: $("#max_context_block"),
             amountGenElem: $("#amount_gen_block"),
@@ -1843,6 +1846,7 @@ function changeMainAPI() {
         "openai": {
             apiSettings: $("#openai_settings"),
             apiConnector: $("#openai_api"),
+            apiPresets: $('#openai_api-presets'),
             apiRanges: $("#range_block_openai"),
             maxContextElem: $("#max_context_block"),
             amountGenElem: $("#amount_gen_block"),
@@ -1859,6 +1863,7 @@ function changeMainAPI() {
         apiObj.apiSettings.css("display", isCurrentApi ? "block" : "none");
         apiObj.apiConnector.css("display", isCurrentApi ? "block" : "none");
         apiObj.apiRanges.css("display", isCurrentApi ? "block" : "none");
+        apiObj.apiPresets.css("display", isCurrentApi ? "block" : "none");
 
         if (isCurrentApi && apiName === "kobold") {
             console.log("enabling SP for kobold");
