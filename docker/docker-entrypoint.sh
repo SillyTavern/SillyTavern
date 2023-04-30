a772cf5aee03818c3566173c0b4ffd664a0ef08b
Grzegorz Gidel <grg.gdl@gmail.com>
2023-04-30 16:22:00 +0200

    Correct check for missing resources in Docker

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
 # Check if the "characters" directory is empty
-if [ -z "$(ls -A /home/node/app/config/characters)" ]; then
-  echo "Characters directory is empty. Copying default characters."
+if [ ! -e "/home/node/app/config/characters" ]; then
+  echo "Characters directory not found. Copying default characters."
   mv /home/node/app/public/characters.default /home/node/app/config/characters
 fi
 
 # Check if the "chats" directory is empty
-if [ -z "$(ls -A /home/node/app/config/chats)" ]; then
-  echo "Chats directory is empty. Copying default chats."
+if [ ! -e "/home/node/app/config/chats" ]; then
+  echo "Chats directory not found. Copying default chats."
   mv /home/node/app/public/chats.default /home/node/app/config/chats/
 fi
 
 # Check if the "User Avatars" directory is empty
-if [ -z "$(ls -A '/home/node/app/config/User Avatars')" ]; then
-  echo "User Avatars directory is empty. Copying default user avatars."
+if [ ! -e "/home/node/app/config/User Avatars" ]; then
+  echo "User Avatars directory not found. Copying default user avatars."
   mv /home/node/app/public/User\ Avatars.default /home/node/app/config/User\ Avatars/
 fi
 
 # Check if the "settings.json" file is not empty
-if [ ! -s "/home/node/app/config/settings.json" ]; then
-  echo "Settings file does not exist. Copying default settings."
+if [ ! -e "/home/node/app/config/settings.json" ]; then
+  echo "Settings file not found. Copying default settings."
   mv /home/node/app/public/settings.json.default /home/node/app/config/settings.json
 fi
 
