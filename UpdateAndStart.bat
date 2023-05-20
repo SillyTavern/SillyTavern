9a537c8a14263913a085df75e8dc4aad3ebe49fd
Sanskar Tiwari <sanskar.mk2@gmail.com>
2023-05-20 21:07:28 +0530

    add flags, lf, eof newline

--- UpdateAndStart.bat
+++ UpdateAndStart.bat
@@ -4,7 +4,7 @@ git --version > nul 2>&1
 if %errorlevel% neq 0 (
     echo Git is not installed on this system. Skipping update.
 ) else (
-    call git pull
+    call git pull --rebase --autostash
     if %errorlevel% neq 0 (
         REM incase we get merge conflicts or something
         echo There were errors while updating. Please download the latest version manually.
