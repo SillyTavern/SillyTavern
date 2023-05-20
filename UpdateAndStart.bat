46e26dbf443e123b25bc017652dfd5f3caad004e
Sanskar Tiwari <sanskar.mk2@gmail.com>
2023-05-20 21:08:04 +0530

    add flags, lf, eof newline

--- UpdateAndStart.bat
+++ UpdateAndStart.bat
@@ -6,7 +6,7 @@ if %errorlevel% neq 0 (
 ) else (
     call git pull --rebase --autostash
     if %errorlevel% neq 0 (
-        REM incase we get merge conflicts or something
+        REM incase there is still something wrong
         echo There were errors while updating. Please download the latest version manually.
     )
 )
