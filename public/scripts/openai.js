d1548ac894ba5fcaf2f802f869ed7d02f770231e
maver <kentucky@posteo.de>
2023-05-28 15:58:12 +0200

    Check oai settings for empty prompt manager configuration

--- public/scripts/openai.js
+++ public/scripts/openai.js
@@ -1128,6 +1128,12 @@ function loadOpenAISettings(data, settings) {
     oai_settings.chat_completion_source = settings.chat_completion_source ?? default_settings.chat_completion_source;
     oai_settings.api_url_scale = settings.api_url_scale ?? default_settings.api_url_scale;
 
+    if (0 === settings.prompts.length) oai_settings.prompts = default_settings.prompts;
+    else oai_settings.prompts = settings.prompts
+
+    oai_settings.prompt_lists = settings.prompt_lists ?? [];
+    oai_settings.prompt_manager_settings = settings.prompt_manager_settings ?? [];
+
     if (settings.nsfw_toggle !== undefined) oai_settings.nsfw_toggle = !!settings.nsfw_toggle;
     if (settings.keep_example_dialogue !== undefined) oai_settings.keep_example_dialogue = !!settings.keep_example_dialogue;
     if (settings.enhance_definitions !== undefined) oai_settings.enhance_definitions = !!settings.enhance_definitions;
