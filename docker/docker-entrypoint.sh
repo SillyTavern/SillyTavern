0dbf219116be92eb11ea7df1a1abfdc1aeb4905e
Cohee <titan0599@gmail.com>
2023-06-03 17:03:57 +0300

    Add worlds folder to docker #434

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Initialize missing user files
-IFS="," RESOURCES="characters,groups,group chats,chats,User Avatars,settings.json"
+IFS="," RESOURCES="characters,groups,group chats,chats,User Avatars,worlds,settings.json"
 for R in $RESOURCES; do
   if [ ! -e "config/$R" ]; then
     echo "Resource not found, copying from defaults: $R"
