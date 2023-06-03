f395c99a5da931a1126073601c01b55f8070ffa2
maver <kentucky@posteo.de>
2023-06-03 02:16:46 +0200

    Remove obsolete default settings in prompt manager constructor

--- public/scripts/openai.js
+++ public/scripts/openai.js
@@ -279,7 +279,7 @@ function setupOpenAIPromptManager(settings) {
         saveSettingsDebounced();
     }
 
-    promptManager.init(configuration, settings, default_settings);
+    promptManager.init(configuration, settings);
     promptManager.render();
 
 
