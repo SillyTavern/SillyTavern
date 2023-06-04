eb01bcb7c12b7a587f0e21bb4c6ad95d78a4a6f1
Cohee <titan0599@gmail.com>
2023-06-04 21:41:22 +0300

    Delete all group chats with a group.

--- public/scripts/group-chats.js
+++ public/scripts/group-chats.js
@@ -1005,7 +1005,7 @@ function select_group_chats(groupId, skipAnimation) {
         }
 
         $("#dialogue_popup").data("group_id", groupId);
-        callPopup("<h3>Delete the group?</h3>", "del_group");
+        callPopup('<h3>Delete the group?</h3><p>This will also delete all your chats with that group. If you want to delete a single conversation, select a "View past chats" option in the lower left menu.</p>', "del_group");
     });
 
     updateFavButtonState(group?.fav ?? false);
