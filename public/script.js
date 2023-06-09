1c1c9d05bf7dfe189827a62688ee2d0f9eda1fe8
maver <kentucky@posteo.de>
2023-06-09 18:50:50 +0200

    Allow generate to run without sending a request

--- public/script.js
+++ public/script.js
@@ -2087,7 +2087,7 @@ class StreamingProcessor {
     }
 }
 
-async function Generate(type, { automatic_trigger, force_name2, resolve, reject, quiet_prompt, force_chid, signal } = {}) {
+async function Generate(type, { automatic_trigger, force_name2, resolve, reject, quiet_prompt, force_chid, signal } = {}, dryRun = false) {
     //console.log('Generate entered');
     setGenerationProgress(0);
     tokens_already_generated = 0;
@@ -2165,7 +2165,8 @@ async function Generate(type, { automatic_trigger, force_name2, resolve, reject,
         return;
     }
 
-    if (online_status != 'no_connection' && this_chid != undefined && this_chid !== 'invalid-safety-id') {
+    if (true === dryRun ||
+        (online_status != 'no_connection' && this_chid != undefined && this_chid !== 'invalid-safety-id')) {
         let textareaText;
         if (type !== 'regenerate' && type !== "swipe" && type !== 'quiet' && !isImpersonate) {
             is_send_press = true;
@@ -2682,6 +2683,8 @@ async function Generate(type, { automatic_trigger, force_name2, resolve, reject,
             } else if (main_api == 'poe') {
                 generate_data = { prompt: finalPromt };
             }
+
+            if (true === dryRun) return onSuccess({error: 'dryRun'});
 
             if (power_user.console_log_prompts) {
 
