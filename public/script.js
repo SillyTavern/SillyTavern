c2ecf9628106e0ffa7ef8541e809fee073f9eb1c
maver <kentucky@posteo.de>
2023-06-10 20:07:55 +0200

    Fire event after a chat has successfully loaded

--- public/script.js
+++ public/script.js
@@ -4198,6 +4198,7 @@ async function getChat() {
         }
         await getChatResult();
         await saveChat();
+        eventSource.emit('chatLoaded', {detail: {id: this_chid, character: characters[this_chid]}});
 
 
         setTimeout(function () {
