28fa2f5f57f6c73b2f17ae75bc41e9df05e53f9b
maver <kentucky@posteo.de>
2023-06-24 19:55:39 +0200

    Render prompt manager after swiping with updated tokens
    Including world info and extension prompts

--- public/scripts/openai.js
+++ public/scripts/openai.js
@@ -610,6 +610,7 @@ function prepareOpenAIMessages({
         }
     } finally {
         promptManager.populateTokenHandler(chatCompletion.getMessages());
+        promptManager.render(false);
     }
 
     const chat = chatCompletion.getChat();
@@ -1073,6 +1074,10 @@ class TokenHandler {
 
     getCounts() {
         return this.counts;
+    }
+
+    resetCounts() {
+        Object.keys(this.counts).forEach((key) => this.counts[key] = 0 );
     }
 
     setCounts(counts) {
