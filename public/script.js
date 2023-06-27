06f580ed2903a3af77c898e5ed1d6e3c3190c711
50h100a <all_awful@proton.me>
2023-06-26 21:45:15 -0400

    Add power-user option to toggle card filenames. Rephrased 'resize' option so It's Not Talking Like This.

--- public/script.js
+++ public/script.js
@@ -226,6 +226,7 @@ export {
     extension_prompt_types,
     updateVisibleDivs,
     mesForShowdownParse,
+    printCharacters,
 }
 
 // API OBJECT FOR EXTERNAL WIRING
@@ -803,7 +804,9 @@ async function printCharacters() {
         template.find('img').attr('src', this_avatar);
         template.find('.avatar').attr('title', item.avatar);
         template.find('.ch_name').text(item.name);
-        template.find('.ch_avatar_url').text(item.avatar);
+        if (power_user.show_card_avatar_urls) {
+            template.find('.ch_avatar_url').text(item.avatar);
+        }
         template.find('.ch_fav_icon').css("display", 'none');
         template.toggleClass('is_fav', item.fav || item.fav == 'true');
         template.find('.ch_fav').val(item.fav);
