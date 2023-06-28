7c6c2ee8b60459ff5828dd1232496acc9393b91f
Cohee <titan0599@gmail.com>
2023-06-29 01:09:34 +0300

    Fix Poe

--- src/poe-client.js
+++ src/poe-client.js
@@ -369,13 +369,13 @@ class Client {
         const nextData = JSON.parse(jsonText);
 
         this.formkey = extractFormKey(r.data);
-        this.viewer = nextData.props.pageProps.payload.viewer;
+        this.viewer = nextData.props.pageProps.data.viewer;
 
         return nextData;
     }
 
     async get_bots() {
-        const viewer = this.next_data.props.pageProps.payload.viewer;
+        const viewer = this.next_data.props.pageProps.data.viewer;
         if (!viewer.availableBotsConnection) {
             throw new Error('Invalid token.');
         }
@@ -398,7 +398,7 @@ class Client {
                         cached_bots[url] = r;
                     }
 
-                    const chatData = r.data.pageProps.payload.chatOfBotDisplayName;
+                    const chatData = r.data.pageProps.data.chatOfBotDisplayName;
                     bots[chatData.defaultBotObject.nickname] = chatData;
                     resolve();
 
