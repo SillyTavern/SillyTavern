6dcdd9112371621d7bf7023376f330a224648775
maver <kentucky@posteo.de>
2023-06-29 19:34:01 +0200

    Add event for modifying settings before modifications

--- public/script.js
+++ public/script.js
@@ -4896,6 +4896,9 @@ async function getSettings(type) {
             }
         }
 
+        // Allow subscribers to mutate settings before applying any modifiers
+        eventSource.emit(event_types.SETTINGS_LOADED_BEFORE, settings);
+
         //Load KoboldAI settings
         koboldai_setting_names = data.koboldai_setting_names;
         koboldai_settings = data.koboldai_settings;
