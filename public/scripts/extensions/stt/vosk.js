0e3f8db898ca86e190d60e4518b4db9f1cc0b7c2
Tony Ribeiro <tonyribeiro.research.aca@gmail.com>
2023-07-09 01:16:49 +0200

    Cleanup remaining useless code for vosk module. Added option for recording mode: auto-send, replace, append. Added push to talk option. Auto-send mode can use given trigger words. Added a debug pannel showing usefull feedback for auto-send mode. Pretty much done, just need more testing

--- public/scripts/extensions/stt/vosk.js
+++ public/scripts/extensions/stt/vosk.js
@@ -15,25 +15,17 @@ class VoskSttProvider {
 
     get settingsHtml() {
         let html = ""
-        /*
-        let html = `
-        <label for="vosk_stt_model_path">Vosk model path:</label>
-        <input id="vosk_stt_model_path" type="text" class="text_pole" maxlength="250" value="${this.defaultSettings.model_path}"/>
-        <span>
-        <span>Use absolute path.</span>
-        `*/
         return html
     }
 
     onSettingsChange() {
         // Used when provider settings are updated from UI
-        //this.settings.model_path = $('#vosk_stt_model_path').val()
     }
 
     loadSettings(settings) {
         // Pupulate Provider UI given input settings
         if (Object.keys(settings).length == 0) {
-            console.info("Using default vosk STT extension settings")
+            console.debug("<STT-Vosk-module> Using default vosk STT extension settings")
         }
 
         // Only accept keys defined in defaultSettings
@@ -47,12 +39,7 @@ class VoskSttProvider {
             }
         }
 
-        //if (!modules.includes('vosk-stt')) {
-        //    console.debug("Error, module vosk-stt must be load by SillyTavern-extra")
-        //}
-
-        //$('#vosk_stt_model_path').val(this.settings.model_path)
-        console.info("Vosk STT settings loaded")
+        console.debug("<STT-Vosk-module> Vosk STT settings loaded")
     }
 
     /*
@@ -65,18 +52,12 @@ class VoskSttProvider {
     //###########//
 
     async getUserMessage() {
-        console.info("Recording user message")
+        console.debug("<STT-Vosk-module> Recording user message")
         // Return if module is not loaded
         if (!modules.includes('vosk-stt')) {
-            console.log("Module vosk-stt must be activated in SillytaverExtra for streaming user voice.")
+            console.debug("<STT-Vosk-module> Module vosk-stt must be activated in SillytaverExtra for streaming user voice.")
             return "";
         }
-
-        // Return if no path to model given
-        //if(this.settings.model_path == "") {
-        //    console.log("No path to model given")
-        //    return ""
-        //}
     
         const url = new URL(getApiUrl());
         url.pathname = '/api/stt/vosk/record';
@@ -98,64 +79,4 @@ class VoskSttProvider {
         const data = await apiResult.json();
         return data.transcript;
     }
-
-    /*
-    async loadModel() {
-        console.info("Loading vosk model")
-        // Return if module is not loaded
-        if (!modules.includes('vosk-stt')) {
-            console.log("Module vosk-stt must be activated in SillytaverExtra for loading vosk model.")
-            return "";
-        }
-        
-        this.settings.model_path = $('#vosk_stt_model_path').val()
-
-        const url = new URL(getApiUrl());
-        url.pathname = '/api/vosk-stt/load';
-    
-        const apiResult = await doExtrasFetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Bypass-Tunnel-Reminder': 'bypass',
-            },
-            body: JSON.stringify({ model_path: this.settings.model_path }),
-        });
-
-        if (!apiResult.ok) {
-            toastr.error(response.statusText, 'Vosk STT model load Failed');
-            throw new Error(`HTTP ${response.status}: ${await response.text()}`);
-        }
-        
-
-        const data = await apiResult.json();
-        return data.transcript;
-    }
-
-    async enabled(value) {
-        const url = new URL(getApiUrl());
-        
-        url.pathname = '/api/vosk-stt/enable';
-
-        if (!value)
-            url.pathname = '/api/vosk-stt/disable';
-    
-        const apiResult = await doExtrasFetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Bypass-Tunnel-Reminder': 'bypass',
-            },
-            body: JSON.stringify({ text: "" }),
-        });
-
-        if (!apiResult.ok) {
-            if (value)
-                toastr.error(response.statusText, 'Vosk STT enable failed');
-            else
-                toastr.error(response.statusText, 'Vosk STT disable failed');
-            throw new Error(`HTTP ${response.status}: ${await response.text()}`);
-        }
-    }
-    */
 }
