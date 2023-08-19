49c6676f72dbe4e8c898c151f33856594517ae3d
SDS <52386626+StefanDanielSchwarz@users.noreply.github.com>
2023-08-19 13:54:09 +0200

    End Prompt with Newline
    This change adds a trailing newline to the instruct mode prompt when "Wrap Sequences with Newline" is on and "Include Names" is off. (When "Include Names" is on, "Wrap Sequences with Newline" is already applied before.)

--- public/scripts/power-user.js
+++ public/scripts/power-user.js
@@ -1099,7 +1099,7 @@ export function formatInstructModePrompt(name, isImpersonate, promptBias, name1,
         text += (includeNames ? promptBias : (separator + promptBias));
     }
 
-    return text.trimEnd();
+    return text.trimEnd() + (includeNames ? '' : separator);
 }
 
 const sortFunc = (a, b) => power_user.sort_order == 'asc' ? compareFunc(a, b) : compareFunc(b, a);
