ea72584f392356e17af6d8d4b6fcadb6a9e4a906
maver <kentucky@posteo.de>
2023-08-20 19:46:46 +0200

    Add elvis to getTokens when populating legacy context.

--- public/scripts/PromptManager.js
+++ public/scripts/PromptManager.js
@@ -1194,7 +1194,7 @@ PromptManagerModule.prototype.populateTokenCounts = function (messages) {
 PromptManagerModule.prototype.populateLegacyTokenCounts = function (messages) {
     // Update general token counts
     const chatHistory = messages.getItemByIdentifier('chatHistory');
-    const startChat = chatHistory?.getCollection()[0].getTokens() || 0;
+    const startChat = chatHistory?.getCollection()[0]?.getTokens() || 0;
     const continueNudge = chatHistory?.getCollection().find(message => message.identifier === 'continueNudge')?.getTokens() || 0;
 
     this.tokenHandler.counts = {
