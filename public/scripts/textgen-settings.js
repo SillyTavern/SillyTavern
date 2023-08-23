fad6c164cbc252760ef2e6febbe8a763d0cded51
Cohee <18619528+Cohee1207@users.noreply.github.com>
2023-08-23 17:44:38 +0300

    Don't set negative prompt from CFG extension to ooba at guidance scale 1.0

--- public/scripts/textgen-settings.js
+++ public/scripts/textgen-settings.js
@@ -233,9 +233,13 @@ async function generateTextGenWithStreaming(generate_data, signal) {
     }
 }
 
-export function getTextGenGenerationData(finalPromt, this_amount_gen, isImpersonate, cfgValues) {
+export function getTextGenGenerationData(finalPrompt, this_amount_gen, isImpersonate, cfgValues) {
+    if (cfgValues?.guidanceScale?.value === 1) {
+        cfgValues = null;
+    }
+
     return {
-        'prompt': finalPromt,
+        'prompt': finalPrompt,
         'max_new_tokens': this_amount_gen,
         'do_sample': textgenerationwebui_settings.do_sample,
         'temperature': textgenerationwebui_settings.temp,
