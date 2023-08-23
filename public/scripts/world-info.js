1ce848c1c322ee6459fe11d638a0ac612718dbf0
Cohee <18619528+Cohee1207@users.noreply.github.com>
2023-08-24 00:26:47 +0300

    Move before / after char WI to story strings

--- public/scripts/world-info.js
+++ public/scripts/world-info.js
@@ -1155,8 +1155,8 @@ async function checkWorldInfo(chat, maxContext) {
         }
     });
 
-    const worldInfoBefore = WIBeforeEntries.length ? `${WIBeforeEntries.join("\n")}\n` : '';
-    const worldInfoAfter = WIAfterEntries.length ? `${WIAfterEntries.join("\n")}\n` : '';
+    const worldInfoBefore = WIBeforeEntries.length ? WIBeforeEntries.join("\n") : '';
+    const worldInfoAfter = WIAfterEntries.length ? WIAfterEntries.join("\n") : '';
 
     if (shouldWIAddPrompt) {
         const originalAN = context.extensionPrompts[NOTE_MODULE_NAME].value;
