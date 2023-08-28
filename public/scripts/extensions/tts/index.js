d8843274b1969a993bc03b7ca7fabcfbd0672ada
ouoertheo <jamin@tomeofjamin.net>
2023-08-27 20:47:44 -0500

    merge voicemap to settings rather than overwrite

--- public/scripts/extensions/tts/index.js
+++ public/scripts/extensions/tts/index.js
@@ -678,7 +678,7 @@ function updateVoiceMap() {
         voiceMap = tempVoiceMap
         console.log(`Voicemap updated to ${JSON.stringify(voiceMap)}`)
     }
-    extension_settings.tts[ttsProviderName].voiceMap = voiceMap
+    Object.assign(extension_settings.tts[ttsProviderName].voiceMap, voiceMap)
     saveSettingsDebounced()
 }
 
