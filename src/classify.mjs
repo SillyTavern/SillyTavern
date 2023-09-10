c76c76410c3a1bc67d97894e19138380599c4e6c
Cohee <18619528+Cohee1207@users.noreply.github.com>
2023-09-11 01:25:22 +0300

    Add ability to override local classification model

--- src/classify.mjs
+++ src/classify.mjs
@@ -1,21 +1,36 @@
-import { pipeline, TextClassificationPipeline, env } from 'sillytavern-transformers';
+import { pipeline, env } from 'sillytavern-transformers';
 import path from 'path';
 
+// Limit the number of threads to 1 to avoid issues on Android
 env.backends.onnx.wasm.numThreads = 1;
 
 class PipelineAccessor {
     /**
-     * @type {TextClassificationPipeline}
+     * @type {import("sillytavern-transformers").TextClassificationPipeline}
      */
     pipe;
 
     async get() {
         if (!this.pipe) {
             const cache_dir = path.join(process.cwd(), 'cache');
-            this.pipe = await pipeline('text-classification', 'Cohee/distilbert-base-uncased-go-emotions-onnx', { cache_dir, quantized: true });
+            const model = this.getClassificationModel();
+            this.pipe = await pipeline('text-classification', model, { cache_dir, quantized: true });
         }
 
         return this.pipe;
+    }
+
+    getClassificationModel() {
+        const DEFAULT_MODEL = 'Cohee/distilbert-base-uncased-go-emotions-onnx';
+
+        try {
+            const config = require(path.join(process.cwd(), './config.conf'));
+            const model = config?.extras?.classificationModel;
+            return model || DEFAULT_MODEL;
+        } catch {
+            console.warn('Failed to read config.conf, using default classification model.');
+            return DEFAULT_MODEL;
+        }
     }
 }
 
@@ -28,30 +43,40 @@ function registerEndpoints(app, jsonParser) {
     const pipelineAccessor = new PipelineAccessor();
 
     app.post('/api/extra/classify/labels', jsonParser, async (req, res) => {
-        const pipe = await pipelineAccessor.get();
-        const result = Object.keys(pipe.model.config.label2id);
-        return res.json({ labels: result });
+        try {
+            const pipe = await pipelineAccessor.get();
+            const result = Object.keys(pipe.model.config.label2id);
+            return res.json({ labels: result });
+        } catch (error) {
+            console.error(error);
+            return res.sendStatus(500);
+        }
     });
 
     app.post('/api/extra/classify', jsonParser, async (req, res) => {
-        const { text } = req.body;
+        try {
+            const { text } = req.body;
 
-        async function getResult(text) {
-            if (cacheObject.hasOwnProperty(text)) {
-                return cacheObject[text];
-            } else {
-                const pipe = await pipelineAccessor.get();
-                const result = await pipe(text);
-                cacheObject[text] = result;
-                return result;
+            async function getResult(text) {
+                if (cacheObject.hasOwnProperty(text)) {
+                    return cacheObject[text];
+                } else {
+                    const pipe = await pipelineAccessor.get();
+                    const result = await pipe(text);
+                    cacheObject[text] = result;
+                    return result;
+                }
             }
+
+            console.log('Classify input:', text);
+            const result = await getResult(text);
+            console.log('Classify output:', result);
+
+            return res.json({ classification: result });
+        } catch (error) {
+            console.error(error);
+            return res.sendStatus(500);
         }
-
-        console.log('Classify input:', text);
-        const result = await getResult(text);
-        console.log('Classify output:', result);
-
-        return res.json({ classification: result });
     });
 }
 
