0f329d2ecbfbe11913cf0c8a3140aef902ef9866
city-unit <140349364+city-unit@users.noreply.github.com>
2023-09-14 23:06:17 -0400

    Comments and cleaup

--- public/scripts/extensions/idle/index.js
+++ public/scripts/extensions/idle/index.js
@@ -3,10 +3,7 @@ import {
     event_types,
     saveSettingsDebounced
 } from "../../../script.js";
-import { selected_group } from "../../group-chats.js";
 import { debounce } from "../../utils.js";
-import { loadMovingUIState } from '../../power-user.js';
-import { dragElement } from '../../RossAscends-mods.js';
 import { sendMessageAsQuiet } from "../../slash-commands.js";
 import { extension_settings, getContext } from "../../extensions.js";
 
@@ -18,105 +15,124 @@ let repeatCount = 0;
 
 let defaultSettings = {
     enabled: false,
-    timer: 30000,
+    timer: 120,
     prompts: [
-        "*{{user}} stands silently, looking deep in thought*",
-        "*{{user}} pauses, eyes wandering over the surroundings*",
-        "*{{user}} hesitates, appearing lost for a moment*",
-        "*{{user}} takes a deep breath, collecting their thoughts*",
-        "*{{user}} gazes into the distance, seemingly distracted*",
-        "*{{user}} remains still, absorbing the ambiance*",
-        "*{{user}} lingers in silence, a contemplative look on their face*",
-        "*{{user}} stops, fingers brushing against an old memory*",
-        "*{{user}} seems to drift into a momentary daydream*",
-        "*{{user}} waits quietly, allowing the weight of the moment to settle*",
+        "*stands silently, looking deep in thought*",
+        "*pauses, eyes wandering over the surroundings*",
+        "*hesitates, appearing lost for a moment*",
+        "*takes a deep breath, collecting their thoughts*",
+        "*gazes into the distance, seemingly distracted*",
+        "*remains still, absorbing the ambiance*",
+        "*lingers in silence, a contemplative look on their face*",
+        "*stops, fingers brushing against an old memory*",
+        "*seems to drift into a momentary daydream*",
+        "*waits quietly, allowing the weight of the moment to settle*",
     ],
     useContinuation: true,
     repeats: 2, // 0 = infinite
-    sendAs : "User"
+    sendAs : "user"
 };
 
+
+//TODO: Can we make this a generic function?
+/**
+ * Load the extension settings and set defaults if they don't exist.
+ */
 async function loadSettings() {
-    // Ensure extension_settings.idle exists
     if (!extension_settings.idle) {
         console.log("Creating extension_settings.idle");
         extension_settings.idle = {};
     }
-
-    // Check and merge each default setting if it doesn't exist
     for (const [key, value] of Object.entries(defaultSettings)) {
         if (!extension_settings.idle.hasOwnProperty(key)) {
             console.log(`Setting default for: ${key}`);
             extension_settings.idle[key] = value;
         }
     }
+    populateUIWithSettings();
+}
 
-    // Update UI components
+//TODO: Can we make this a generic function too?
+/**
+ * Populate the UI components with values from the extension settings.
+ */
+function populateUIWithSettings() {
     $("#idle_timer").val(extension_settings.idle.timer).trigger("input");
-    //Turn our array into a newline separated string
     $("#idle_Prompts").val(extension_settings.idle.prompts.join("\n")).trigger("input");
     $("#idle_use_continuation").prop("checked", extension_settings.idle.useContinuation).trigger("input");
     $("#idle_enabled").prop("checked", extension_settings.idle.enabled).trigger("input");
     $("#idle_repeats").val(extension_settings.idle.repeats).trigger("input");
     $("#idle_sendAs").val(extension_settings.idle.sendAs).trigger("input");
-
 }
 
 
-// Function to reset the timer
+/**
+ * Reset the idle timer based on the extension settings and context.
+ */
 function resetIdleTimer() {
     console.log("Resetting idle timer");
     if (idleTimer) clearTimeout(idleTimer);
     let context = getContext();
     console.debug(context);
     if (!context.characterId && !context.groupID) return;
-    idleTimer = setTimeout(sendIdlePrompt, extension_settings.idle.timer);
+    idleTimer = setTimeout(sendIdlePrompt, 1000*extension_settings.idle.timer);
 }
 
-// Function to send a random idle prompt to the AI
+/**
+ * Send a random idle prompt to the AI based on the extension settings.
+ * Checks conditions like if the extension is enabled and repeat conditions.
+ */
 async function sendIdlePrompt() {
-    console.debug(extension_settings.idle.repeats, repeatCount);
-    if(!extension_settings.idle.enabled) return;
+    if (!extension_settings.idle.enabled) return;
 
-    if(extension_settings.idle.repeats > 0 && repeatCount >= extension_settings.idle.repeats) return;
-
-    //If we are waiting for a response, don't send an idle prompt
-    if ($('#mes_stop').is(':visible')) {
-        console.log("Not sending idle prompt, waiting for response");
+    // Check repeat conditions and waiting for a response
+    if ((extension_settings.idle.repeats > 0 && repeatCount >= extension_settings.idle.repeats)
+        || $('#mes_stop').is(':visible')) {
+        console.debug("Not sending idle prompt due to repeat conditions or waiting for a response.");
         resetIdleTimer();
         return;
     }
 
-    const prompts = extension_settings.idle.prompts;
-    const randomPrompt = prompts[Math.floor(Math.random() * prompts.length)];
-    //pause the timer
+    const randomPrompt = extension_settings.idle.prompts[
+        Math.floor(Math.random() * extension_settings.idle.prompts.length)
+    ];
+
+    sendPrompt(randomPrompt);
+    repeatCount++;
+    resetIdleTimer();
+}
+
+/**
+ * Send the provided prompt to the AI. Determines method based on continuation setting.
+ * @param {string} prompt - The prompt text to send to the AI.
+ */
+function sendPrompt(prompt) {
     clearTimeout(idleTimer);
-    //stop listening for send_textarea input
     $("#send_textarea").off("input");
+
     if (extension_settings.idle.useContinuation) {
         $('#option_continue').trigger('click');
         console.log("Sending idle prompt with continuation");
+    } else {
+        console.debug("Sending idle prompt");
+        sendMessageAsQuiet(extension_settings.idle.sendAs, prompt);
     }
-    else {
-        console.log("Sending idle prompt");
-        sendMessageAsQuiet(extension_settings.idle.sendAs, randomPrompt);
-    }
-    repeatCount++;
-    resetIdleTimer();
-    // Re-register the event listener
-    $("#send_textarea").on("input", function () {
-        console.log("send_textarea input");
-        resetIdleTimer();
-    }
-    );
 }
 
-
+/**
+ * Load the settings HTML and append to the designated area.
+ */
 async function loadSettingsHTML() {
-    const settingsHtml = await $.get("scripts/extensions/idle/dropdown.html");
+    const settingsHtml = await $.get(`${extensionFolderPath}dropdown.html`);
     $("#extensions_settings2").append(settingsHtml);
 }
 
+/**
+ * Update a specific setting based on user input.
+ * @param {string} elementId - The HTML element ID tied to the setting.
+ * @param {string} property - The property name in the settings object.
+ * @param {boolean} [isCheckbox=false] - Whether the setting is a checkbox.
+ */
 function updateSetting(elementId, property, isCheckbox = false) {
     let value = $(`#${elementId}`).val();
     if (isCheckbox) {
@@ -131,12 +147,22 @@ function updateSetting(elementId, property, isCheckbox = false) {
     saveSettingsDebounced();
 }
 
+/**
+ * Attach an input listener to a UI component to update the corresponding setting.
+ * @param {string} elementId - The HTML element ID tied to the setting.
+ * @param {string} property - The property name in the settings object.
+ * @param {boolean} [isCheckbox=false] - Whether the setting is a checkbox.
+ */
 function attachUpdateListener(elementId, property, isCheckbox = false) {
     $(`#${elementId}`).on('input', debounce(() => {
         updateSetting(elementId, property, isCheckbox);
-    }, 250)); // Debounce with 250ms delay
+    }, 250));
 }
 
+/**
+ * Handle the enabling or disabling of the idle extension.
+ * Clears or resets the timer based on the extension's enabled status.
+ */
 function handleIdleEnabled() {
     if (!extension_settings.idle.enabled) {
         clearTimeout(idleTimer);
@@ -145,23 +171,28 @@ function handleIdleEnabled() {
     }
 }
 
+
+/**
+ * Setup input listeners for the various settings and actions related to the idle extension.
+ */
 function setupListeners() {
-    // Attach listeners for settings
-    attachUpdateListener('idle_timer', 'timer');
-    attachUpdateListener('idle_Prompts', 'prompts');
-    attachUpdateListener('idle_use_continuation', 'useContinuation', true);
-    attachUpdateListener('idle_enabled', 'enabled', true);
-    attachUpdateListener('idle_repeats', 'repeats');
+    const settingsToWatch = [
+        ['idle_timer', 'timer'],
+        ['idle_Prompts', 'prompts'],
+        ['idle_use_continuation', 'useContinuation', true],
+        ['idle_enabled', 'enabled', true],
+        ['idle_repeats', 'repeats']
+    ];
+    settingsToWatch.forEach(setting => {
+        attachUpdateListener(...setting);
+    });
 
-    // Handle special case for idle_enabled
+    // Idleness listeners, could be made better
     $('#idle_enabled').on('input', debounce(handleIdleEnabled, 250));
-
-    // Attach resetIdleTimer to multiple events
     $("#send_textarea, #send_but").on("input click", debounce(resetIdleTimer, 250));
     $(document).on("click keypress", debounce(resetIdleTimer, 250));
     document.addEventListener('keydown', debounce(resetIdleTimer, 250));
-
-    eventSource.on(event_types.CHARACTER_MESSAGE_RENDERED, debounce(function (e) {
+    eventSource.on(event_types.CHARACTER_MESSAGE_RENDERED, debounce(e => {
         resetIdleTimer();
         repeatCount = 0;
     }, 250));
@@ -171,11 +202,7 @@ jQuery(async () => {
     await loadSettingsHTML();
     loadSettings();
     setupListeners();
-
-    // Call it once initially to start the timer if enabled
-
-    if (extension_settings.idle.enabled)
-    {
+    if (extension_settings.idle.enabled) {
         resetIdleTimer();
     }
 });
