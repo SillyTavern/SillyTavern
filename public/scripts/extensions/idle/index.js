4f993a69f197e4982ab9909e3fba86738059d0d2
city-unit <140349364+city-unit@users.noreply.github.com>
2023-09-17 14:54:59 -0400

    Fix repeat logic to prompt once then only repeat the number specified

--- public/scripts/extensions/idle/index.js
+++ public/scripts/extensions/idle/index.js
@@ -99,8 +99,7 @@ async function sendIdlePrompt() {
     if (!extension_settings.idle.enabled) return;
 
     // Check repeat conditions and waiting for a response
-    if ((extension_settings.idle.repeats > 0 && repeatCount >= extension_settings.idle.repeats)
-        || $('#mes_stop').is(':visible')) {
+    if (repeatCount > extension_settings.idle.repeats || $('#mes_stop').is(':visible')) {
         //console.debug("Not sending idle prompt due to repeat conditions or waiting for a response.");
         resetIdleTimer();
         return;
