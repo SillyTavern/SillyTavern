2b40fc7e760ca8fd7ad83d0c7e543cfe63093e4c
Tony Ribeiro <tonyribeiro.research.aca@gmail.com>
2023-10-24 15:44:40 +0200

    Use proper path function for live2d asset of character folder

--- src/assets.js
+++ src/assets.js
@@ -257,7 +257,7 @@ function registerEndpoints(app, jsonParser) {
                             for (let file of fs.readdirSync(live2dModelPath)) {
                                 //console.debug("Character live2d model found:", file)
                                 if (file.includes("model"))
-                                    output.push([`${modelFolder}`,`/characters/${name}/${category}/${modelFolder}/${file}`]);
+                                    output.push([`${modelFolder}`,path.join("characters", name, category, modelFolder, file) ]);
                             }
                         }
                     }
