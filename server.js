091255d451f2d4e542a9f19e642f26f55a9deb91
valadaptive <valadaptive@protonmail.com>
2023-12-04 12:55:13 -0500

    Use Express router for secrets endpoint

--- server.js
+++ server.js
@@ -3585,7 +3585,7 @@ require('./src/endpoints/tokenizers').registerEndpoints(app, jsonParser);
 require('./src/endpoints/presets').registerEndpoints(app, jsonParser);
 
 // Secrets managemenet
-require('./src/endpoints/secrets').registerEndpoints(app, jsonParser);
+app.use('/api/secrets', require('./src/endpoints/secrets').router);
 
 // Thumbnail generation
 require('./src/endpoints/thumbnails').registerEndpoints(app, jsonParser);
