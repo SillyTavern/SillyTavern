d24c1dde10355000ef84dd5a23ee782941823927
valadaptive <valadaptive@protonmail.com>
2023-12-04 12:40:53 -0500

    Use Express router for assets + "files" endpoints
    I've split out the "file/upload" endpoint into its own module, and
    renamed it to "files" to be consistent with the existing naming scheme.

---
+++ src/endpoints/files.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const writeFileSyncAtomic = require('write-file-atomic').sync;
+const express = require('express');
+const router = express.Router();
+const { checkAssetFileName } = require('./assets');
+const { jsonParser } = require('../express-common');
+const { DIRECTORIES } = require('../constants');
+
+router.post('/upload', jsonParser, async (request, response) => {
+    try {
+        if (!request.body.name) {
+            return response.status(400).send('No upload name specified');
+        }
+
+        if (!request.body.data) {
+            return response.status(400).send('No upload data specified');
+        }
+
+        const safeInput = checkAssetFileName(request.body.name);
+
+        if (!safeInput) {
+            return response.status(400).send('Invalid upload name');
+        }
+
+        const pathToUpload = path.join(DIRECTORIES.files, safeInput);
+        writeFileSyncAtomic(pathToUpload, request.body.data, 'base64');
+        const url = path.normalize(pathToUpload.replace('public' + path.sep, ''));
+        return response.send({ path: url });
+    } catch (error) {
+        console.log(error);
+        return response.sendStatus(500);
+    }
+});
+
+module.exports = { router };
