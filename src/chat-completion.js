2a035786057af665b2f6732b8543f68f6054a0a6
DonMoralez <donmoralez@gmail.com>
2023-12-06 23:00:42 +0200

    сhanged prefix assignment and modified syspromt

--- src/chat-completion.js
+++ src/chat-completion.js
@@ -8,68 +8,56 @@
  * @copyright Prompt Conversion script taken from RisuAI by kwaroran (GPLv3).
  */
 function convertClaudePrompt(messages, addHumanPrefix, addAssistantPostfix, withSystemPrompt) {
-    // Claude doesn't support message names, so we'll just add them to the message content.
-    for (const message of messages) {
-        if (message.name && message.role !== 'system') {
-            message.content = message.name + ': ' + message.content;
-            delete message.name;
-        }
-    }
-
-    let systemPrompt = '';
-    if (withSystemPrompt) {
-        let lastSystemIdx = -1;
-
-        for (let i = 0; i < messages.length - 1; i++) {
-            const message = messages[i];
-            if (message.role === 'system' && !message.name) {
-                systemPrompt += message.content + '\n\n';
-            } else {
-                lastSystemIdx = i - 1;
-                break;
-            }
-        }
-        if (lastSystemIdx >= 0) {
-            messages.splice(0, lastSystemIdx + 1);
-        }
-    }
-
-    let requestPrompt = messages.map((v) => {
-        let prefix = '';
-        switch (v.role) {
-            case 'assistant':
-                prefix = '\n\nAssistant: ';
-                break;
-            case 'user':
-                prefix = '\n\nHuman: ';
-                break;
-            case 'system':
-                // According to the Claude docs, H: and A: should be used for example conversations.
-                if (v.name === 'example_assistant') {
-                    prefix = '\n\nA: ';
-                } else if (v.name === 'example_user') {
-                    prefix = '\n\nH: ';
-                } else {
-                    prefix = '\n\n';
-                }
-                break;
-        }
-        return prefix + v.content;
+	// Find the index of the first message with an assistant role and check for a "'user' role/Human:" before it.
+	const firstAssistantIndex = messages.findIndex(message => message.role === "assistant");
+	const hasUser = messages.slice(0, firstAssistantIndex).some(message => message.role === "user" || message.content.includes("Human:"));
+	
+	let requestPrompt = messages.map((v, i) => {
+		// Claude doesn't support message names, so we'll just add them to the message content.
+		// Now iside the same cycle.
+		if (v.name && v.role !== "system") {
+			v.content = `${v.name}: ${v.content}`;
+			delete v.name;
+		}
+		
+		let prefix = '';
+		// Define role prefixes(role : prefix).
+		const rolePrefixes = {
+			"assistant": "\n\nAssistant: ",
+			"user": "\n\nHuman: ",
+			"system": v.name === "example_assistant" ? "\n\nA: " : v.name === "example_user" ? "\n\nH: " : "\n\n"
+		}
+		// For Claude 2.1, add "/n/n" as prefix when the role of the first message is 'system' or 'assistant'. 
+		// Otherwise, add "Human: " prefix when the role is 'user' (for compatibility with 2.0 format). 		
+		if (i === 0 && ["system", "assistant"].includes(v.role) && withSystemPrompt) {
+			prefix = "\n\n";
+		// If there is no message with role "user" or prefix "Human:", change the first assistant's prefix.(insert the human's message).
+		} else if (i === firstAssistantIndex && !hasUser && withSystemPrompt) {
+			prefix = "\n\nHuman: Let's get started.\n\nAssistant: ";
+		// For Claude 2.0, add the "Human:" prefix to the first message.
+		} else if (i === 0 && addHumanPrefix) {
+			prefix = "\n\nHuman: ";
+		// Set the correct prefix according to the role.
+		} else {
+			prefix = rolePrefixes[v.role] || '\n\n';
+		}
+		return prefix + v.content;
     }).join('');
-
-    if (addHumanPrefix) {
-        requestPrompt = '\n\nHuman: ' + requestPrompt;
-    }
-
-    if (addAssistantPostfix) {
-        requestPrompt = requestPrompt + '\n\nAssistant: ';
-    }
-
-    if (withSystemPrompt) {
-        requestPrompt = systemPrompt + requestPrompt;
-    }
-
-    return requestPrompt;
+	
+	// Another way to insert 'Human'.
+	// If there is no message with role "user" or prefix "Human: ", add the new message before the first message of the assistant.
+	// Remember to comment or remove .join('') above before use.
+	/**
+	if (withSystemPrompt && !hasUser && firstAssistantIndex > -1) {
+		requestPrompt.splice(firstAssistantIndex, 0, "\n\nHuman: Let's get started.");
+	}
+	requestPrompt = requestPrompt.join('');
+	*/
+	if (addAssistantPostfix) {
+		requestPrompt += "\n\nAssistant: ";
+	}
+	
+	return requestPrompt;
 }
 
 module.exports = {
