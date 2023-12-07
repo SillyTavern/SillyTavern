990f958f4f605c1eff29e75606c0dd1263d667a0
Cohee <18619528+Cohee1207@users.noreply.github.com>
2023-12-07 16:59:53 +0200

    #1484 Consolidate chat name template

--- public/script.js
+++ public/script.js
@@ -1291,7 +1291,7 @@ async function replaceCurrentChat() {
 
         // start new chat
         else {
-            characters[this_chid].chat = name2 + ' - ' + humanizedDateTime();
+            characters[this_chid].chat = `${name2} - ${humanizedDateTime()}`;
             $('#selected_chat_pole').val(characters[this_chid].chat);
             saveCharacterDebounced();
             await getChat();
@@ -8026,7 +8026,7 @@ jQuery(async function () {
             else {
                 //RossAscends: added character name to new chat filenames and replaced Date.now() with humanizedDateTime;
                 chat_metadata = {};
-                characters[this_chid].chat = name2 + '-' + humanizedDateTime();
+                characters[this_chid].chat = `${name2} - ${humanizedDateTime()}`;
                 $('#selected_chat_pole').val(characters[this_chid].chat);
                 await getChat();
                 await createOrEditCharacter();
