499d158c11136c8649c7bb3eb75dcdd31de2f54c
valadaptive <valadaptive@protonmail.com>
2023-12-09 20:55:34 -0500

    Remove last usage of getAPIServerUrl
    Now that we're not using this in the tokenizers code, we can remove it.

--- public/script.js
+++ public/script.js
@@ -871,7 +871,7 @@ async function getStatusKobold() {
 
     const url = '/getstatus';
 
-    let endpoint = getAPIServerUrl();
+    let endpoint = api_server;
 
     if (!endpoint) {
         console.warn('No endpoint for status check');
@@ -919,7 +919,9 @@ async function getStatusKobold() {
 async function getStatusTextgen() {
     const url = '/api/textgenerationwebui/status';
 
-    let endpoint = getAPIServerUrl();
+    let endpoint = textgen_settings.type === MANCER ?
+        MANCER_SERVER :
+        api_server_textgenerationwebui;
 
     if (!endpoint) {
         console.warn('No endpoint for status check');
@@ -997,23 +999,6 @@ export function stopStatusLoading() {
 export function resultCheckStatus() {
     displayOnlineStatus();
     stopStatusLoading();
-}
-
-// TODO(valadaptive): remove the usage of this function in the tokenizers code, then remove the function entirely
-export function getAPIServerUrl() {
-    if (main_api == 'textgenerationwebui') {
-        if (textgen_settings.type === MANCER) {
-            return MANCER_SERVER;
-        }
-
-        return api_server_textgenerationwebui;
-    }
-
-    if (main_api == 'kobold') {
-        return api_server;
-    }
-
-    return '';
 }
 
 export async function selectCharacterById(id) {
