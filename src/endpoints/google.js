bb8b8f9386ebecbecef5764e64688aa96802413d
Cohee <18619528+Cohee1207@users.noreply.github.com>
2023-12-14 20:36:31 +0200

    Fix sending PNG/WEBP to Google captioning

--- src/endpoints/google.js
+++ src/endpoints/google.js
@@ -8,20 +8,25 @@ const router = express.Router();
 
 router.post('/caption-image', jsonParser, async (request, response) => {
     try {
+        const mimeType = request.body.image.split(';')[0].split(':')[1];
+        const base64Data = request.body.image.split(',')[1];
         const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision:generateContent?key=${readSecret(SECRET_KEYS.MAKERSUITE)}`;
         const body = {
             contents: [{
                 parts: [
                     { text: request.body.prompt },
-                    { inlineData: {
-                        mimeType: 'image/png', //jpg images seem to work fine even with this mimetype set?
-                        data: request.body.image,
-                    },
+                    {
+                        inlineData: {
+                            mimeType: 'image/png', // It needs to specify a MIME type in data if it's not a PNG
+                            data: mimeType === 'image/png' ? base64Data : request.body.image,
+                        },
                     }],
             }],
             safetySettings: MAKERSUITE_SAFETY,
             generationConfig: { maxOutputTokens: 1000 },
         };
+
+        console.log('Multimodal captioning request', body);
 
         const result = await fetch(url, {
             body: JSON.stringify(body),
@@ -32,10 +37,9 @@ router.post('/caption-image', jsonParser, async (request, response) => {
             timeout: 0,
         });
 
-        console.log('Multimodal captioning request', body);
-
         if (!result.ok) {
-            console.log(`MakerSuite API returned error: ${result.status} ${result.statusText} ${await result.text()}`);
+            const error = await result.json();
+            console.log(`MakerSuite API returned error: ${result.status} ${result.statusText}`, error);
             return response.status(result.status).send({ error: true });
         }
 
@@ -43,7 +47,7 @@ router.post('/caption-image', jsonParser, async (request, response) => {
         console.log('Multimodal captioning response', data);
 
         const candidates = data?.candidates;
-        if(!candidates) {
+        if (!candidates) {
             return response.status(500).send('No candidates found, image was most likely filtered.');
         }
 
