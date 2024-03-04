1cce26d631460cbf638182f1deef26086cc025e4
Cohee <18619528+Cohee1207@users.noreply.github.com>
2024-03-04 22:24:50 +0200

    #1891 Fix JWT Scale generation

--- public/script.js
+++ public/script.js
@@ -4511,6 +4511,10 @@ function parseAndSaveLogprobs(data, continueFrom) {
  * @returns {string} Extracted message
  */
 function extractMessageFromData(data) {
+    if (typeof data === 'string') {
+        return data;
+    }
+
     switch (main_api) {
         case 'kobold':
             return data.results[0].text;
