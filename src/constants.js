9cb777a4e92da2f69f0480af8189bb4f99f3b556
Cohee <18619528+Cohee1207@users.noreply.github.com>
2024-04-14 17:36:48 +0300

    Increase default avatar size 400x600 => 512x768

--- src/constants.js
+++ src/constants.js
@@ -243,8 +243,8 @@ const OLLAMA_KEYS = [
     'mirostat_eta',
 ];
 
-const AVATAR_WIDTH = 400;
-const AVATAR_HEIGHT = 600;
+const AVATAR_WIDTH = 512;
+const AVATAR_HEIGHT = 768;
 
 const OPENROUTER_HEADERS = {
     'HTTP-Referer': 'https://sillytavern.app',
