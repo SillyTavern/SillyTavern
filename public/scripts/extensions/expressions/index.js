366e2abb8ba4ef38a01fea6de0a267de21dc6feb
Cohee <18619528+Cohee1207@users.noreply.github.com>
2024-04-19 23:01:31 +0300

    Move expressions LLM request setting mode listener

--- public/scripts/extensions/expressions/index.js
+++ public/scripts/extensions/expressions/index.js
@@ -1076,6 +1076,7 @@ async function getExpressionLabel(text) {
             case EXPRESSION_API.llm: {
                 const expressionsList = await getExpressionsList();
                 const prompt = await getLlmPrompt(expressionsList);
+                eventSource.once(event_types.TEXT_COMPLETION_SETTINGS_READY, onTextGenSettingsReady);
                 const emotionResponse = await generateQuietPrompt(prompt, false, false);
                 return parseLlmResponse(emotionResponse, expressionsList);
             }
@@ -1956,7 +1957,6 @@ function migrateSettings() {
     });
     eventSource.on(event_types.MOVABLE_PANELS_RESET, updateVisualNovelModeDebounced);
     eventSource.on(event_types.GROUP_UPDATED, updateVisualNovelModeDebounced);
-    eventSource.on(event_types.TEXT_COMPLETION_SETTINGS_READY, onTextGenSettingsReady);
     registerSlashCommand('sprite', setSpriteSlashCommand, ['emote'], '<span class="monospace">(spriteId)</span> – force sets the sprite for the current character', true, true);
     registerSlashCommand('spriteoverride', setSpriteSetCommand, ['costume'], '<span class="monospace">(optional folder)</span> – sets an override sprite folder for the current character. If the name starts with a slash or a backslash, selects a sub-folder in the character-named folder. Empty value to reset to default.', true, true);
     registerSlashCommand('lastsprite', (_, value) => lastExpression[value.trim()] ?? '', [], '<span class="monospace">(charName)</span> – Returns the last set sprite / expression for the named character.', true, true);
