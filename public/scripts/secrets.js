2b1aee9e71dfb7e9ec3753ffe80d5f5361ca2fcb
Yokayo <52032299+Yokayo@users.noreply.github.com>
2024-04-24 21:07:42 +0700

    Localize two hard-coded strings

--- public/scripts/secrets.js
+++ public/scripts/secrets.js
@@ -62,10 +62,11 @@ async function clearSecret() {
     $('#main_api').trigger('change');
 }
 
-function updateSecretDisplay() {
+export function updateSecretDisplay() {
     for (const [secret_key, input_selector] of Object.entries(INPUT_MAP)) {
         const validSecret = !!secret_state[secret_key];
-        const placeholder = validSecret ? '✔️ Key saved' : '❌ Missing key';
+		
+		const placeholder = $('#viewSecrets').attr(validSecret ? 'key_saved_text' : 'missing_key_text');
         $(input_selector).attr('placeholder', placeholder);
     }
 }
