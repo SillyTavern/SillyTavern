64500bfb37cad492761accd21a58c55bbc2425ef
Cohee <18619528+Cohee1207@users.noreply.github.com>
2024-05-13 16:48:35 +0300

    Add rounding to textarea autofit

--- public/scripts/RossAscends-mods.js
+++ public/scripts/RossAscends-mods.js
@@ -702,7 +702,7 @@ const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
  */
 function autoFitSendTextArea() {
     const originalScrollBottom = chatBlock.scrollHeight - (chatBlock.scrollTop + chatBlock.offsetHeight);
-    if (sendTextArea.scrollHeight + 3 == sendTextArea.offsetHeight) {
+    if (Math.ceil(sendTextArea.scrollHeight + 3) >= Math.floor(sendTextArea.offsetHeight)) {
         // Needs to be pulled dynamically because it is affected by font size changes
         const sendTextAreaMinHeight = window.getComputedStyle(sendTextArea).getPropertyValue('min-height');
         sendTextArea.style.height = sendTextAreaMinHeight;
