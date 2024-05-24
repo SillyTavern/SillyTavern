da4f0f53be7865130ba3c4309a348e79123f2fdb
Cohee <18619528+Cohee1207@users.noreply.github.com>
2024-05-24 23:41:27 +0300

    Resolve char macros to message name in first message display

--- public/script.js
+++ public/script.js
@@ -1791,7 +1791,7 @@ export function messageFormatting(mes, ch_name, isSystem, isUser, messageId) {
     }
 
     if (Number(messageId) === 0 && !isSystem && !isUser) {
-        mes = substituteParams(mes);
+        mes = substituteParams(mes, undefined, ch_name);
     }
 
     mesForShowdownParse = mes;
