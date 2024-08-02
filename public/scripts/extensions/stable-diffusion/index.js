d84fef047821d2202cda629735e4ddcc8e9ff7d6
Wolfsblvt <wolfsblvt@gmail.com>
2024-08-03 00:10:46 +0200

    Fix SD wand entry not always being clickable

--- public/scripts/extensions/stable-diffusion/index.js
+++ public/scripts/extensions/stable-diffusion/index.js
@@ -3396,7 +3396,7 @@ async function addSDGenButtons() {
     $(document).on('click touchend', function (e) {
         const target = $(e.target);
         if (target.is(dropdown) || target.closest(dropdown).length) return;
-        if (target.is(button) && !dropdown.is(':visible') && $('#send_but').is(':visible')) {
+        if ((target.is(button) || target.closest(button).length) && !dropdown.is(':visible') && $('#send_but').is(':visible')) {
             e.preventDefault();
 
             dropdown.fadeIn(animation_duration);
