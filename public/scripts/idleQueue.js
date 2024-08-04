0d6338faad890cd9694f87ad8244a5532d1b76c1
steve02081504 <steve02081504@EDEN.fukwold>
2024-08-04 13:41:11 +0800

    more idle

--- public/scripts/idleQueue.js
+++ public/scripts/idleQueue.js
@@ -18,11 +18,11 @@ function resetTimer() {
 }
 
 window.addEventListener('load', resetTimer, true);
-window.addEventListener('mousemove', resetTimer, true);
+// window.addEventListener('mousemove', resetTimer, true);
 window.addEventListener('mousedown', resetTimer, true);
 window.addEventListener('touchstart', resetTimer, true);
 window.addEventListener('touchmove', resetTimer, true);
 window.addEventListener('click', resetTimer, true);
 window.addEventListener('keydown', resetTimer, true);
-window.addEventListener('scroll', resetTimer, true);
-window.addEventListener('wheel', resetTimer, true);
+// window.addEventListener('scroll', resetTimer, true);
+// window.addEventListener('wheel', resetTimer, true);
