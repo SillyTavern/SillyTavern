5281bb9b5941945fa333c48a82e9c9ef81c85c8a
刘悦 <zcxey2911@hotmail.com>
2024-09-08 15:23:48 +0800

    Update index.js

--- public/scripts/extensions/tts/index.js
+++ public/scripts/extensions/tts/index.js
@@ -15,6 +15,7 @@ import { VITSTtsProvider } from './vits.js';
 import { GSVITtsProvider } from './gsvi.js';
 import { SBVits2TtsProvider } from './sbvits2.js';
 import { AllTalkTtsProvider } from './alltalk.js';
+import { CosyVoiceProvider } from './cosyvoice.js';
 import { SpeechT5TtsProvider } from './speecht5.js';
 import { AzureTtsProvider } from './azure.js';
 import { SlashCommandParser } from '../../slash-commands/SlashCommandParser.js';
@@ -98,6 +99,7 @@ const ttsProviders = {
     System: SystemTtsProvider,
     VITS: VITSTtsProvider,
     XTTSv2: XTTSTtsProvider,
+    CosyVoice: CosyVoiceProvider,
 };
 let ttsProvider;
 let ttsProviderName;
