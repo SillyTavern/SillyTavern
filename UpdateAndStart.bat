6c110e91f7769f0c12990cf12f61875b3daf8989
Cohee <18619528+Cohee1207@users.noreply.github.com>
2024-10-01 12:32:32 +0300

    Exit scripts when try to update non-git installs

--- UpdateAndStart.bat
+++ UpdateAndStart.bat
@@ -2,9 +2,15 @@
 pushd %~dp0
 git --version > nul 2>&1
 if %errorlevel% neq 0 (
-    echo Git is not installed on this system. Skipping update.
-    echo If you installed with a zip file, you will need to download the new zip and install it manually.
+    echo [91mGit is not installed on this system.[0m
+    echo Install it from https://git-scm.com/downloads
+    goto end
 ) else (
+    if not exist .git (
+        echo [91mNot running from a Git repository. Reinstall using an officially supported method to get updates.[0m
+        echo See: https://docs.sillytavern.app/installation/windows/
+        goto end
+    )
     call git pull --rebase --autostash
     if %errorlevel% neq 0 (
         REM incase there is still something wrong
@@ -14,5 +20,6 @@ if %errorlevel% neq 0 (
 set NODE_ENV=production
 call npm install --no-audit --no-fund --loglevel=error --no-progress --omit=dev
 node server.js %*
+:end
 pause
 popd
