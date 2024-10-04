38875fb9403aca9892675d5122e72d617bf06baf
M0cho <77959408+M0ch0@users.noreply.github.com>
2024-10-04 20:16:24 +0900

    Supports Gemini context length reduction.

--- public/scripts/openai.js
+++ public/scripts/openai.js
@@ -4009,8 +4009,6 @@ async function onModelChange() {
             $('#openai_max_context').attr('max', max_2mil);
         } else if (value.includes('gemini-1.5-pro')) {
             $('#openai_max_context').attr('max', max_2mil);
-        } else if (value.match('gemini-1.5-flash-002')) {
-            $('#openai_max_context').attr('max', max_2mil);
         } else if (value.includes('gemini-1.5-flash')) {
             $('#openai_max_context').attr('max', max_1mil);
         } else if (value.includes('gemini-1.0-pro-vision') || value === 'gemini-pro-vision') {
