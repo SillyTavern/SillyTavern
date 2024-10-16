e4959c6ccb50ec8a7b2753d2aab938bc61d28e9b
Cohee <18619528+Cohee1207@users.noreply.github.com>
2024-10-17 00:49:06 +0300

    webpack: Reduce logging

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,12 @@ export const publicLibConfig = {
     cache: true,
     devtool: 'source-map',
     module: {},
+    stats: 'minimal',
     experiments: {
         outputModule: true,
+    },
+    performance: {
+        hints: false,
     },
     output: {
         filename: 'lib.js',
