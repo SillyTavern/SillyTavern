7b00547e1bd65be77ea1bdcc8891f2937bdfe136
Reithan <bo122081@hotmail.com>
2025-02-27 22:27:05 -0800

    regex wizardry no recursion

--- public/scripts/variables.js
+++ public/scripts/variables.js
@@ -238,25 +238,25 @@ export function resolveVariable(name, scope = null) {
 export function getVariableMacros() {
     return [
         // Replace {{setvar::name::value}} with empty string and set the variable name to value
-        { regex: /{{setvar::([^:]+)::([^}]+)}}/gi, replace: (_, name, value) => { setLocalVariable(name.trim(), value); return ''; } },
+        { regex: /{{setvar::([a-zA-Z0-9]+?)::((?:{{.*?}}|[^{}])+?)}}/gi, replace: (_, name, value) => { setLocalVariable(name.trim(), value); return ''; } },
         // Replace {{addvar::name::value}} with empty string and add value to the variable value
-        { regex: /{{addvar::([^:]+)::([^}]+)}}/gi, replace: (_, name, value) => { addLocalVariable(name.trim(), value); return ''; } },
+        { regex: /{{addvar::([a-zA-Z0-9]+?)::((?:{{.*?}}|[^{}])+?)}}/gi, replace: (_, name, value) => { addLocalVariable(name.trim(), value); return ''; } },
         // Replace {{incvar::name}} with empty string and increment the variable name by 1
-        { regex: /{{incvar::([^}]+)}}/gi, replace: (_, name) => incrementLocalVariable(name.trim()) },
+        { regex: /{{incvar::([a-zA-Z0-9]+?)}}/gi, replace: (_, name) => incrementLocalVariable(name.trim()) },
         // Replace {{decvar::name}} with empty string and decrement the variable name by 1
-        { regex: /{{decvar::([^}]+)}}/gi, replace: (_, name) => decrementLocalVariable(name.trim()) },
+        { regex: /{{decvar::([a-zA-Z0-9]+?)}}/gi, replace: (_, name) => decrementLocalVariable(name.trim()) },
         // Replace {{getvar::name}} with the value of the variable name
-        { regex: /{{getvar::([^}]+)}}/gi, replace: (_, name) => getLocalVariable(name.trim()) },
+        { regex: /{{getvar::([a-zA-Z0-9]+?)}}/gi, replace: (_, name) => getLocalVariable(name.trim()) },
         // Replace {{setglobalvar::name::value}} with empty string and set the global variable name to value
-        { regex: /{{setglobalvar::([^:]+)::([^}]+)}}/gi, replace: (_, name, value) => { setGlobalVariable(name.trim(), value); return ''; } },
+        { regex: /{{setglobalvar::([a-zA-Z0-9]+?)::((?:{{.*?}}|[^{}])+)}}/gi, replace: (_, name, value) => { setGlobalVariable(name.trim(), value); return ''; } },
         // Replace {{addglobalvar::name::value}} with empty string and add value to the global variable value
-        { regex: /{{addglobalvar::([^:]+)::([^}]+)}}/gi, replace: (_, name, value) => { addGlobalVariable(name.trim(), value); return ''; } },
+        { regex: /{{addglobalvar::([a-zA-Z0-9]+?)::((?:{{.*?}}|[^{}])+)}}/gi, replace: (_, name, value) => { addGlobalVariable(name.trim(), value); return ''; } },
         // Replace {{incglobalvar::name}} with empty string and increment the global variable name by 1
-        { regex: /{{incglobalvar::([^}]+)}}/gi, replace: (_, name) => incrementGlobalVariable(name.trim()) },
+        { regex: /{{incglobalvar::([a-zA-Z0-9]+?)}}/gi, replace: (_, name) => incrementGlobalVariable(name.trim()) },
         // Replace {{decglobalvar::name}} with empty string and decrement the global variable name by 1
-        { regex: /{{decglobalvar::([^}]+)}}/gi, replace: (_, name) => decrementGlobalVariable(name.trim()) },
+        { regex: /{{decglobalvar::([a-zA-Z0-9]+?)}}/gi, replace: (_, name) => decrementGlobalVariable(name.trim()) },
         // Replace {{getglobalvar::name}} with the value of the global variable name
-        { regex: /{{getglobalvar::([^}]+)}}/gi, replace: (_, name) => getGlobalVariable(name.trim()) },
+        { regex: /{{getglobalvar::([a-zA-Z0-9]+?)}}/gi, replace: (_, name) => getGlobalVariable(name.trim()) },
     ];
 }
 
