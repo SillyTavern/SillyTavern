d675183052fad671b8e2e9dc0a3c1983fc2abacf
Reithan <bo122081@hotmail.com>
2025-02-27 22:36:34 -0800

    reorder to better support setting in different forms

--- public/scripts/variables.js
+++ public/scripts/variables.js
@@ -237,26 +237,26 @@ export function resolveVariable(name, scope = null) {
  */
 export function getVariableMacros() {
     return [
-        // Replace {{setvar::name::value}} with empty string and set the variable name to value
-        { regex: /{{setvar::([a-zA-Z0-9]+?)::((?:{{.*?}}|[^{}])+?)}}/gi, replace: (_, name, value) => { setLocalVariable(name.trim(), value); return ''; } },
-        // Replace {{addvar::name::value}} with empty string and add value to the variable value
-        { regex: /{{addvar::([a-zA-Z0-9]+?)::((?:{{.*?}}|[^{}])+?)}}/gi, replace: (_, name, value) => { addLocalVariable(name.trim(), value); return ''; } },
+        // Replace {{getvar::name}} with the value of the variable name
+        { regex: /{{getvar::([a-zA-Z0-9]+?)}}/gi, replace: (_, name) => getLocalVariable(name.trim()) },
+        // Replace {{getglobalvar::name}} with the value of the global variable name
+        { regex: /{{getglobalvar::([a-zA-Z0-9]+?)}}/gi, replace: (_, name) => getGlobalVariable(name.trim()) },
         // Replace {{incvar::name}} with empty string and increment the variable name by 1
         { regex: /{{incvar::([a-zA-Z0-9]+?)}}/gi, replace: (_, name) => incrementLocalVariable(name.trim()) },
         // Replace {{decvar::name}} with empty string and decrement the variable name by 1
         { regex: /{{decvar::([a-zA-Z0-9]+?)}}/gi, replace: (_, name) => decrementLocalVariable(name.trim()) },
-        // Replace {{getvar::name}} with the value of the variable name
-        { regex: /{{getvar::([a-zA-Z0-9]+?)}}/gi, replace: (_, name) => getLocalVariable(name.trim()) },
-        // Replace {{setglobalvar::name::value}} with empty string and set the global variable name to value
-        { regex: /{{setglobalvar::([a-zA-Z0-9]+?)::((?:{{.*?}}|[^{}])+)}}/gi, replace: (_, name, value) => { setGlobalVariable(name.trim(), value); return ''; } },
-        // Replace {{addglobalvar::name::value}} with empty string and add value to the global variable value
-        { regex: /{{addglobalvar::([a-zA-Z0-9]+?)::((?:{{.*?}}|[^{}])+)}}/gi, replace: (_, name, value) => { addGlobalVariable(name.trim(), value); return ''; } },
         // Replace {{incglobalvar::name}} with empty string and increment the global variable name by 1
         { regex: /{{incglobalvar::([a-zA-Z0-9]+?)}}/gi, replace: (_, name) => incrementGlobalVariable(name.trim()) },
         // Replace {{decglobalvar::name}} with empty string and decrement the global variable name by 1
         { regex: /{{decglobalvar::([a-zA-Z0-9]+?)}}/gi, replace: (_, name) => decrementGlobalVariable(name.trim()) },
-        // Replace {{getglobalvar::name}} with the value of the global variable name
-        { regex: /{{getglobalvar::([a-zA-Z0-9]+?)}}/gi, replace: (_, name) => getGlobalVariable(name.trim()) },
+        // Replace {{setvar::name::value}} with empty string and set the variable name to value
+        { regex: /{{setvar::([a-zA-Z0-9]+?)::((?:{{.*?}}|[^{}])+?)}}/gi, replace: (_, name, value) => { setLocalVariable(name.trim(), value); return ''; } },
+        // Replace {{addvar::name::value}} with empty string and add value to the variable value
+        { regex: /{{addvar::([a-zA-Z0-9]+?)::((?:{{.*?}}|[^{}])+?)}}/gi, replace: (_, name, value) => { addLocalVariable(name.trim(), value); return ''; } },
+        // Replace {{setglobalvar::name::value}} with empty string and set the global variable name to value
+        { regex: /{{setglobalvar::([a-zA-Z0-9]+?)::((?:{{.*?}}|[^{}])+)}}/gi, replace: (_, name, value) => { setGlobalVariable(name.trim(), value); return ''; } },
+        // Replace {{addglobalvar::name::value}} with empty string and add value to the global variable value
+        { regex: /{{addglobalvar::([a-zA-Z0-9]+?)::((?:{{.*?}}|[^{}])+)}}/gi, replace: (_, name, value) => { addGlobalVariable(name.trim(), value); return ''; } },
     ];
 }
 
