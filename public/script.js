05d0a07aefe925a5510d9f42e26d7fffbc1a9c26
Reithan <bo122081@hotmail.com>
2025-03-02 17:45:48 -0800

    one more

--- public/script.js
+++ public/script.js
@@ -4032,7 +4032,7 @@ export async function Generate(type, { automatic_trigger, force_name2, quiet_pro
         flushWIDepthInjections();
         if (Array.isArray(worldInfoDepth)) {
             worldInfoDepth.forEach((e) => {
-                const joinedEntries = e.entries.join('\n');
+                const joinedEntries = e.entries.join("\n\n");
                 setExtensionPrompt(`customDepthWI-${e.depth}-${e.role}`, joinedEntries, extension_prompt_types.IN_CHAT, e.depth, false, e.role);
             });
         }
