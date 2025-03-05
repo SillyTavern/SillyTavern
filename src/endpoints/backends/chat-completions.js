cc88d0713991943a75c710c5b966fe4620bc36ae
Sorro <89468301+Sorro123@users.noreply.github.com>
2025-03-06 00:24:17 +0100

    Add gemini tools support. Changes basically nothing but allows the extension I'm making to work

--- src/endpoints/backends/chat-completions.js
+++ src/endpoints/backends/chat-completions.js
@@ -330,6 +330,7 @@ async function sendMakerSuiteRequest(request, response) {
             contents: prompt.contents,
             safetySettings: safetySettings,
             generationConfig: generationConfig,
+            tools: request.body.tools,
         };
 
         if (should_use_system_prompt) {
