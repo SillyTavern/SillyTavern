0aab68e1aa17bb1619c624e137d87926ce50594e
Cohee <18619528+Cohee1207@users.noreply.github.com>
2025-03-06 19:36:25 +0200

    omit dev dependencies in regular startup scripts

--- src/electron/Start.bat
+++ src/electron/Start.bat
@@ -1,6 +1,6 @@
 @echo off
 pushd %~dp0
-call npm install --no-audit --no-fund --loglevel=error --no-progress
+call npm install --no-audit --no-fund --loglevel=error --no-progress --omit=dev
 npm run start server.js %*
 pause
 popd
