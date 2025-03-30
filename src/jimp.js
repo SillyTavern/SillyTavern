703038c0e1db3a420c8968a687514ac4ff2efa16
Cohee <18619528+Cohee1207@users.noreply.github.com>
2025-03-30 18:07:05 +0300

    Install jimp plugins explicitly

--- src/jimp.js
+++ src/jimp.js
@@ -1,6 +1,4 @@
 import { createJimp } from '@jimp/core';
-import { defaultPlugins } from 'jimp';
-import { JimpMime } from 'jimp';
 
 // Optimized image formats
 import webp from '@jimp/wasm-webp';
@@ -13,11 +11,52 @@ import bmp, { msBmp } from '@jimp/js-bmp';
 import gif from '@jimp/js-gif';
 import tiff from '@jimp/js-tiff';
 
+// Plugins
+import * as blit from '@jimp/plugin-blit';
+import * as circle from '@jimp/plugin-circle';
+import * as color from '@jimp/plugin-color';
+import * as contain from '@jimp/plugin-contain';
+import * as cover from '@jimp/plugin-cover';
+import * as crop from '@jimp/plugin-crop';
+import * as displace from '@jimp/plugin-displace';
+import * as fisheye from '@jimp/plugin-fisheye';
+import * as flip from '@jimp/plugin-flip';
+import * as mask from '@jimp/plugin-mask';
+import * as resize from '@jimp/plugin-resize';
+import * as rotate from '@jimp/plugin-rotate';
+import * as threshold from '@jimp/plugin-threshold';
+import * as quantize from '@jimp/plugin-quantize';
+
+const defaultPlugins = [
+    blit.methods,
+    circle.methods,
+    color.methods,
+    contain.methods,
+    cover.methods,
+    crop.methods,
+    displace.methods,
+    fisheye.methods,
+    flip.methods,
+    mask.methods,
+    resize.methods,
+    rotate.methods,
+    threshold.methods,
+    quantize.methods,
+];
+
 // A custom jimp that uses WASM for optimized formats and JS for the rest
 const Jimp = createJimp({
     formats: [webp, png, jpeg, avif, bmp, msBmp, gif, tiff],
-    plugins: defaultPlugins,
+    plugins: [...defaultPlugins],
 });
+
+const JimpMime = {
+    bmp: bmp().mime,
+    gif: gif().mime,
+    jpeg: jpeg().mime,
+    png: png().mime,
+    tiff: tiff().mime,
+};
 
 export default Jimp;
 
