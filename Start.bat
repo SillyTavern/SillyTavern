aa6de5d478424780236cd16d9f14b75b68580d1e
Paul-16098 <pl816098@gmail.com>
2025-06-18 20:11:35 +0800

    Revert "add i18n(zh-cn,zh-tw)"
    This reverts commit 46a0c739701b5f2c2c9c360c8fbced1ee1f3cd3d.

--- Start.bat
+++ Start.bat
@@ -1,6 +1,7 @@
 @echo off
 pushd %~dp0
 set NODE_ENV=production
-node server.js --files--noSandbox %*
+call npm install --no-audit --no-fund --loglevel=error --no-progress --omit=dev
+node server.js %*
 pause
 popd
