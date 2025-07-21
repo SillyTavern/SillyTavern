8bba186d9974e1096eb6493e276bf685efc1d773
Cohee <18619528+Cohee1207@users.noreply.github.com>
2025-07-21 20:26:18 +0300

    Bump multer field size limit Closes #4291

--- src/server-main.js
+++ src/server-main.js
@@ -229,7 +229,7 @@ app.post('/api/ping', (request, response) => {
 
 // File uploads
 const uploadsPath = path.join(cliArgs.dataRoot, UPLOADS_DIRECTORY);
-app.use(multer({ dest: uploadsPath, limits: { fieldSize: 10 * 1024 * 1024 } }).single('avatar'));
+app.use(multer({ dest: uploadsPath, limits: { fieldSize: 500 * 1024 * 1024 } }).single('avatar'));
 app.use(multerMonkeyPatch);
 
 app.get('/version', async function (_, response) {
