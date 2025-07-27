f95c3841313b8b70f484b6d71fdcb785d435b92b
Aleksander Lygren Toppe <aleksander@toppe.no>
2025-07-27 20:02:18 +0200

    fixes #4324: injectedIndices becomes invalidated with a off-by-one index shift when PHI/jailbreak message is added. Could lead to dropped injections/history.

--- public/script.js
+++ public/script.js
@@ -3796,7 +3796,10 @@ export async function Generate(type, { automatic_trigger, force_name2, quiet_pro
                 coreChat.splice(coreChat.length - 1, 0, { mes: jailbreak, is_user: true });
             }
             else {
+                // This operation will result in the injectedIndices indexes being off by one
                 coreChat.push({ mes: jailbreak, is_user: true });
+                // Add +1 to the elements to correct for the new PHI/Jailbreak message.
+                injectedIndices.forEach((e,idx) => injectedIndices[idx] = e + 1);
             }
         }
     }
