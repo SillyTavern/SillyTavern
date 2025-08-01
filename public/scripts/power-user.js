1a5866807b1ff4832a257ba849b0f1bc88b539c3
route-404-gh <route_404_mail@pm.me>
2025-08-01 12:43:38 -0600

    power user local tag settings

--- public/scripts/power-user.js
+++ public/scripts/power-user.js
@@ -293,6 +293,7 @@ export const power_user = {
     custom_stopping_strings_macro: true,
     fuzzy_search: false,
     encode_tags: false,
+    hide_local_tags: true,
     servers: [],
     bogus_folders: false,
     zoomed_avatar_magnification: false,
@@ -1591,6 +1592,7 @@ export async function loadPowerUserSettings(settings, data) {
     $('#persona_allow_multi_connections').prop('checked', power_user.persona_allow_multi_connections);
     $('#persona_auto_lock').prop('checked', power_user.persona_auto_lock);
     $('#encode_tags').prop('checked', power_user.encode_tags);
+    $('#hide_local_tags').prop('checked', power_user.hide_local_tags);
     $('#example_messages_behavior').val(getExampleMessagesBehavior());
     $(`#example_messages_behavior option[value="${getExampleMessagesBehavior()}"]`).prop('selected', true);
     $('#instruct_derived').parent().find('i').toggleClass('toggleEnabled', !!power_user.instruct_derived);
@@ -3862,6 +3864,12 @@ jQuery(() => {
 
     $('#encode_tags').on('input', async function () {
         power_user.encode_tags = !!$(this).prop('checked');
+        await reloadCurrentChat();
+        saveSettingsDebounced();
+    });
+    
+    $('#hide_local_tags').on('input', async function () {
+        power_user.hide_local_tags = !!$(this).prop('checked');
         await reloadCurrentChat();
         saveSettingsDebounced();
     });
