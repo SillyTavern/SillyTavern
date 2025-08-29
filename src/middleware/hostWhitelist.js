da17b086721a51484ff65848f1a507654a1a2c7b
Cohee <18619528+Cohee1207@users.noreply.github.com>
2025-08-30 01:47:48 +0300

    Add host whitelisting middleware

---
+++ src/middleware/hostWhitelist.js
@@ -0,0 +1,40 @@
+import path from 'node:path';
+import { color, getConfigValue, safeReadFileSync } from '../util.js';
+import { serverDirectory } from '../server-directory.js';
+import { isHostAllowed, hostValidationMiddleware } from 'host-validation-middleware';
+
+const knownHosts = new Set();
+
+const hostWhitelistEnabled = !!getConfigValue('hostWhitelist.enabled', false);
+const hostWhitelist = getConfigValue('hostWhitelist.hosts', []);
+const hostWhitelistScan = !!getConfigValue('hostWhitelist.scan', false, 'boolean');
+
+const hostNotAllowedHtml = safeReadFileSync(path.join(serverDirectory, 'public/error/host-not-allowed.html'))?.toString() ?? '';
+
+const validationMiddleware = hostValidationMiddleware({
+    allowedHosts: hostWhitelist,
+    generateErrorMessage: () => hostNotAllowedHtml,
+    errorResponseContentType: 'text/html',
+});
+
+/**
+ * Middleware to validate remote hosts.
+ * Useful to protect against DNS rebinding attacks.
+ * @param {import('express').Request} req Request
+ * @param {import('express').Response} res Response
+ * @param {import('express').NextFunction} next Next middleware
+ */
+export default function hostWhitelistMiddleware(req, res, next) {
+    const hostValue = req.headers.host;
+    if (hostWhitelistScan && !isHostAllowed(hostValue, hostWhitelist) && !knownHosts.has(hostValue)) {
+        console.warn(color.red('Request from untrusted host:'), hostValue);
+        console.warn(`If you trust this host, you can add it to ${color.yellow('hostWhitelist.whitelist')} in config.yaml`);
+        knownHosts.add(hostValue);
+    }
+
+    if (!hostWhitelistEnabled) {
+        return next();
+    }
+
+    return validationMiddleware(req, res, next);
+}
