fd54acbd1d6ff96185b4d5b702d84f4b0b65ae7f
user <user@exmaple.com>
2025-10-01 12:18:20 -0700

    Fixed "Tree file left behind when renaming chats"
    https://github.com/SillyTavern/SillyTavern/pull/4573#discussion_r2395546142

--- src/endpoints/chats.js
+++ src/endpoints/chats.js
@@ -542,11 +542,11 @@ router.post('/rename', validateAvatarUrlMiddleware, async function (request, res
 
         if (fs.existsSync(pathToOriginalTreeFile)) {
             if (!fs.existsSync(pathToRenamedTreeFile)) {
-                console.warn(`The Destination tree file path is not available. ${pathToRenamedTreeFile}`);
-                // return response.status(400).send({ error: true });
-            } else {
                 fs.copyFileSync(pathToOriginalTreeFile, pathToRenamedTreeFile);
                 fs.unlinkSync(pathToOriginalTreeFile);
+            } else {
+                console.warn(`The Destination tree file path is not available. ${pathToRenamedTreeFile}`);
+                // return response.status(400).send({ error: true });
             }
         }
 
@@ -850,7 +850,7 @@ router.post('/group/save', (request, response) => {
     //Write the chatTree
     if (!isNaN(chatTreeData?.['branch_id'])) {
         //Spaces increase file size.
-        const jsonChatTree = JSON.stringify(chatTreeData);
+        const jsonChatTree = JSON.stringify(chatTreeData, null, 0);
         writeFileAtomicSync(treeFilePath, jsonChatTree, 'utf8');
     }
 
