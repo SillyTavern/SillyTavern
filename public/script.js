968e63e6b4717ac090f413f4188223b057138c54
user <user@exmaple.com>
2025-10-01 11:56:13 -0700

    Reverts change `hideSwipeButtons()` change.
    https://github.com/SillyTavern/SillyTavern/pull/4576#discussion_r2394996102

--- public/script.js
+++ public/script.js
@@ -7876,11 +7876,9 @@ export function showSwipeButtons() {
 }
 
 export function hideSwipeButtons() {
-    const messageElement = chatElement.find(`[mesid="${ chat.length - 1 }"]`);
-
-    messageElement.find('.swipe_right').hide();
-    messageElement.find('.swipes-counter').hide();
-    messageElement.find('.swipe_left').hide();
+    chatElement.find('.swipe_right').hide();
+    chatElement.find('.last_mes .swipes-counter').hide();
+    chatElement.find('.swipe_left').hide();
 }
 
 /**
