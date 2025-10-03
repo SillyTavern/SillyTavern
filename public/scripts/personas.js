dc3c15c031458687aa5e40e814e2373c1b4fb5c1
Wolfsblvt <wolfsblvt@gmail.com>
2025-10-04 00:26:36 +0200

    fix: remove redundant type check in persona connection filtering logic
    (char avatar and group key *should* always be unique and different anyway, no need to check on type. Makes this more flexible)

--- public/scripts/personas.js
+++ public/scripts/personas.js
@@ -1559,7 +1559,7 @@ async function loadPersonaForCurrentChat({ doRender = false } = {}) {
 export function getConnectedPersonas(characterKey = undefined) {
     characterKey ??= selected_group || characters[Number(this_chid)]?.avatar;
     const connectedPersonas = Object.entries(power_user.persona_descriptions)
-        .filter(([_, { connections }]) => connections?.some(conn => conn.type === (selected_group ? 'group' : 'character') && conn.id === characterKey))
+        .filter(([_, { connections }]) => connections?.some(conn => conn.id === characterKey))
         .map(([key, _]) => key);
     return connectedPersonas;
 }
