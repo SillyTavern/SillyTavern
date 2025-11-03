e5f278d368a32d73e8e63ab6cc99f32d4c5832a7
user <user@exmaple.com>
2025-11-02 17:38:05 -0800

    Fixed Off by one error due to `chatElement.children()` selecting `show_more_messages`. https://github.com/SillyTavern/SillyTavern/pull/4712#issuecomment-3478331073

--- public/script.js
+++ public/script.js
@@ -8540,10 +8540,11 @@ export function refreshSwipeButtons() {
     let hideBothElements = new Set(); //Hidden.
 
     // const lasttDisplayedMesId = Number(chatElement.find('.mes').last().attr('mesid'));
-    const firstDisplayedMesId = Number(chatElement.find('.mes').first().attr('mesid'));
+    const messageElements = chatElement.find('.mes[mesid]');
+    const firstDisplayedMesId = Number(messageElements.first().attr('mesid'));
 
     //Group each message.
-    chatElement.children().each((index, div) => {
+    messageElements.each((index, div) => {
         // const messageId = Number($(div).attr('mesid')); //Slower.
         //This assumes the messages are in order and their Id's are accurate.
         const messageId = firstDisplayedMesId + index;
