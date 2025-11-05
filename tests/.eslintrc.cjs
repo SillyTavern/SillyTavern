3194c3954a60da9d9c1b46a80d19ef9198db9e50
bmen25124 <bmen25124@gmail.com>
2025-11-05 14:43:34 +0300

    gangnam style

--- tests/.eslintrc.cjs
+++ tests/.eslintrc.cjs
@@ -10,9 +10,11 @@ module.exports = {
     env: {
         es6: true,
         node: true,
+        'jest/globals': true,
     },
     parserOptions: {
         ecmaVersion: 'latest',
+        sourceType: 'module',
     },
     overrides: [
     ],
