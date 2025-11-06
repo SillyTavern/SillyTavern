aa3edf210e78383799ecd9d2eb143421e8b6ab8d
Cohee <18619528+Cohee1207@users.noreply.github.com>
2025-11-06 22:36:36 +0200

    Conditional include for guided_json

--- public/scripts/textgen-settings.js
+++ public/scripts/textgen-settings.js
@@ -1603,7 +1603,7 @@ export async function getTextGenGenerationData(finalPrompt, maxTokens, isImperso
         'skip_special_tokens': settings.skip_special_tokens,
         'spaces_between_special_tokens': settings.spaces_between_special_tokens,
         'guided_grammar': settings.grammar_string,
-        'guided_json': settings.json_schema,
+        'guided_json': settings.json_schema ? settings.json_schema : undefined,
         'early_stopping': false, // hacks
         'include_stop_str_in_output': false,
         'dynatemp_min': dynatemp ? settings.min_temp : undefined,
