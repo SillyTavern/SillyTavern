547eccf27938ac33c950d307ccd6825053ff4c95
Cohee <18619528+Cohee1207@users.noreply.github.com>
2025-11-17 22:15:38 +0200

    Reduce reset time 500 -> 350

--- public/script.js
+++ public/script.js
@@ -9434,7 +9434,7 @@ export async function swipe(event, direction, { source, repeated, message = chat
      */
     function getSwipeDuration(animation_duration) {
         const now = performance.now();
-        const resetTime = 500;
+        const resetTime = 350;
 
         //Reset the counter if the last swipe was more than half a second ago.
         if (now - lastSwipeTime >= resetTime) heldSwipes = 0;
