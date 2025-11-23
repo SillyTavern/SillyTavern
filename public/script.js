7883b08367b1998d513834270cc15258ff241f7d
user <user@exmaple.com>
2025-11-23 09:06:58 -0800

    Fixed Reduced Motion causing a warning when swiping back.

--- public/script.js
+++ public/script.js
@@ -9561,7 +9561,8 @@ export async function swipe(event, direction, { source, repeated, message = chat
                 //Shake 700/140=5px
                 shakeElement(thisMesDiv, -swipeRange / 140, animation_duration, 'ease-in');
                 //Flash red.
-                await Promise.race([thisMesDiv.find('.swipes-counter').animate({ color: 'red' }, animation_duration * 2).animate({ color: '' }).promise(), createTimeout(animation_duration * 8, `The shake animation did not end within ${animation_duration * 8}ms`)].filter(Boolean));
+                const flashTime = Math.max(animation_duration * 2, 100)
+                await Promise.race([thisMesDiv.find('.swipes-counter').animate({ color: 'red' }, flashTime).animate({ color: '' }).promise(), createTimeout(flashTime * 4, `The shake animation did not end within ${flashTime * 4}ms`)].filter(Boolean));
             } catch (error) {
                 console.warn(error);
             }
