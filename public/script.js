f754335f9bb1f1cf9324a0b6c3e5123089e9ed78
Cohee <18619528+Cohee1207@users.noreply.github.com>
2026-01-08 23:32:07 +0200

    Refactor property access

--- public/script.js
+++ public/script.js
@@ -5143,7 +5143,7 @@ export async function Generate(type, { automatic_trigger, force_name2, quiet_pro
             chatInjects: injectedIndices?.map(index => arrMes[arrMes.length - index - 1])?.join('') || '',
             summarizeString: (extension_prompts['1_memory']?.value || ''),
             authorsNoteString: (extension_prompts['2_floating_prompt']?.value || ''),
-            smartContextString: (extension_prompts.chromadb?.value || ''),
+            smartContextString: (extension_prompts['chromadb']?.value || ''),
             chatVectorsString: (extension_prompts['3_vectors']?.value || ''),
             dataBankVectorsString: (extension_prompts['4_vectors_data_bank']?.value || ''),
             worldInfoString: worldInfoString,
@@ -7387,16 +7387,16 @@ export async function getChat() {
             dataType: 'json',
             contentType: 'application/json',
         });
-        if (response[0] !== undefined) {
-
-            if (response[0].chat_metadata) {
-                /** @type {ChatHeader} */
-                const chatHeader = response.shift();
-                chat_metadata = chatHeader.chat_metadata ?? {};
-            }
+        if (Array.isArray(response) && response.length > 0) {
+            /** @type {ChatHeader} */
+            const chatHeader = response.shift();
+            chat_metadata = chatHeader?.chat_metadata ?? {};
             chat.splice(0, chat.length, ...response);
-
             chat.forEach(ensureMessageMediaIsArray);
+        } else {
+            // An empty/corrupted chat file
+            chat.splice(0, chat.length);
+            chat_metadata = {};
         }
         if (!chat_metadata.integrity) {
             chat_metadata.integrity = uuidv4();
@@ -9914,7 +9914,7 @@ export async function swipe(event, direction, { source, repeated, message = chat
                 };
                 //Wait for the animation's end. https://developer.mozilla.org/en-US/docs/Web/API/Animation/finished
                 const animations = swipedElementsDiv[0]?.getAnimations() ?? [];
-                const animation = animations.filter((a) => a instanceof CSSAnimation && a.animationName == 'slide')[0];
+                const animation = animations.filter((a) => a instanceof globalThis.CSSAnimation && a.animationName == 'slide')[0];
                 try {
                     await Promise.race([animation?.finished, createTimeout(duration * 2, `The ${duration}ms swipe animation has not ended after ${duration * 2}ms. It has been skipped.`)].filter(Boolean));
                 } catch (error) {
