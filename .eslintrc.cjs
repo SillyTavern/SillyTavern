ac677bceacd295938292c262d07c54bfa52a489c
user <user@exmaple.com>
2026-01-20 08:26:44 -0800

    Added 'dot-notation': ['error'], to `.eslint.cjs`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -98,6 +98,7 @@ module.exports = {
         'no-cond-assign': 'error',
         'no-unneeded-ternary': 'error',
         'no-irregular-whitespace': ['error', { skipStrings: true, skipTemplates: true }],
+        'dot-notation': ['error'],
 
         // These rules should eventually be enabled.
         'no-async-promise-executor': 'off',
