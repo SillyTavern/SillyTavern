5b86183c816e219f5ce2a8d30af1bc1d6bd11554
Cohee <18619528+Cohee1207@users.noreply.github.com>
2026-02-16 21:11:37 +0200

    Update help string to clarify image saving behavior in generation requests

--- public/scripts/extensions/stable-diffusion/index.js
+++ public/scripts/extensions/stable-diffusion/index.js
@@ -5562,7 +5562,7 @@ jQuery(async () => {
         ],
         helpString: `
             <div>
-                Requests to generate an image and posts it to chat (unless <code>quiet=true</code> argument is specified). The image is saved to the character gallery by default; use <code>gallery=false</code> to skip saving.
+                Requests to generate an image and posts it to chat (unless <code>quiet=true</code> argument is specified). The image is saved to the character gallery by default; use <code>gallery=false</code> to save to the root of the user images directory.
             </div>
             <div>
                 Supported arguments: <code>${Object.values(triggerWords).flat().join(', ')}</code>.
