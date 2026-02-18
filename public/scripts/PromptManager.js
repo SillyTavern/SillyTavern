5c32154fa498d9bdf3e74143dd1a8154b4300690
Tosd0 <65720409+Sevenyine@users.noreply.github.com>
2026-02-19 02:02:40 +0800

    Update translations

--- public/scripts/PromptManager.js
+++ public/scripts/PromptManager.js
@@ -1300,7 +1300,7 @@ class PromptManager {
         const prompt = this.getPromptById(identifier);
         const textareaIdentifier = `${identifier}_prompt_quick_edit_textarea`;
         const html = `<div class="range-block m-t-1">
-                        <div class="justifyLeft" data-i18n="${title}">${title}</div>
+                        <div class="justifyLeft">${title}</div>
                         <div class="wide100p">
                             <textarea id="${textareaIdentifier}" class="text_pole textarea_compact" rows="6" placeholder="">${prompt.content}</textarea>
                         </div>
