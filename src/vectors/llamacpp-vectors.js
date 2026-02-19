c304c474951d456b3b9e2f32ee5d26932d9758c2
Cohee <18619528+Cohee1207@users.noreply.github.com>
2026-02-19 23:00:42 +0200

    Trim trailing v1 from URL

--- src/vectors/llamacpp-vectors.js
+++ src/vectors/llamacpp-vectors.js
@@ -2,6 +2,7 @@ import fetch from 'node-fetch';
 import urlJoin from 'url-join';
 import { setAdditionalHeadersByType } from '../additional-headers.js';
 import { TEXTGEN_TYPES } from '../constants.js';
+import { trimV1 } from '../util.js';
 
 /**
  * Gets the vector for the given text from LlamaCpp
@@ -11,7 +12,7 @@ import { TEXTGEN_TYPES } from '../constants.js';
  * @returns {Promise<number[][]>} - The array of vectors for the texts
  */
 export async function getLlamaCppBatchVector(texts, apiUrl, directories) {
-    const url = new URL(urlJoin(apiUrl, 'v1/embeddings'));
+    const url = new URL(urlJoin(trimV1(apiUrl), '/v1/embeddings'));
 
     const headers = {};
     setAdditionalHeadersByType(headers, TEXTGEN_TYPES.LLAMACPP, apiUrl, directories);
