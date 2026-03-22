e4aef38857122925ae90db2c15f94ff3701886c6
user <user@exmaple.com>
2026-03-21 17:10:48 -0700

    Stopping strings should only apply to Text Completions https://github.com/SillyTavern/SillyTavern/pull/5337#pullrequestreview-3987164461

--- public/script.js
+++ public/script.js
@@ -3744,7 +3744,8 @@ class StreamingProcessor {
         // when streaming, we cache the result of getStoppingStrings instead of calling it once per token.
         const isImpersonate = this.type == 'impersonate';
         const isContinue = this.type == 'continue';
-        this.stoppingStrings = getStoppingStrings(isImpersonate, isContinue);
+        //Stopping strings should only apply to Text Completions.
+        if (main_api !== 'openai') this.stoppingStrings = getStoppingStrings(isImpersonate, isContinue);
 
         try {
             const sw = new Stopwatch(1000 / power_user.streaming_fps);
