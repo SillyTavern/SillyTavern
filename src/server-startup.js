6ab40ce4762ce578f12e513c1ae86212a89f1b65
Cohee <18619528+Cohee1207@users.noreply.github.com>
2026-03-24 23:25:37 +0200

    Update language in getAddressInUseMessage

--- src/server-startup.js
+++ src/server-startup.js
@@ -236,7 +236,7 @@ export class ServerStartup {
      */
     #getAddressInUseMessage(url, ipVersion) {
         const listenAddress = this.#getListenAddress(url, ipVersion);
-        return `Error: Port ${listenAddress} is already in use. Another SillyTavern instance may already be running. Stop the other process or change "port" in config.yaml.`;
+        return `Address ${listenAddress} is already in use. Another SillyTavern instance may already be running. Stop the other process or change "port" in config.yaml.`;
     }
 
     /**
