12c8bb75d34b3716a331cc5cafcaf081564ced6c
Cohee <18619528+Cohee1207@users.noreply.github.com>
2026-03-31 20:05:58 +0300

    fix: update event parameter types in onSwipeCounterClick and addLongPressEvent

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -2955,7 +2955,7 @@ export function createTimeout(ms, errorMessage = '') {
  * Registers a long-press (touch hold) event as an alternative to modifier+click.
  * Supports event delegation for dynamically created elements.
  * @param {string} selector CSS selector for target elements
- * @param {function} callback Callback to invoke on long-press, `this` is the matched element
+ * @param {(e: TouchEvent) => void} callback Callback to invoke on long-press, `this` is the matched element
  * @param {number} [delay=500] Long-press duration in ms
  */
 export function addLongPressEvent(selector, callback, delay = 500) {
