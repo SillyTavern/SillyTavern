72dfad456c24c82cd8da37a9e56f9b07a7c7ba69
Cohee <18619528+Cohee1207@users.noreply.github.com>
2026-04-01 23:24:29 +0300

    feat: add --ignore-scripts flag to npm install commands in batch and shell scripts

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ fi
 
 echo "Installing Node Modules..."
 export NODE_ENV=production
-npm i --no-save --no-audit --no-fund --loglevel=error --no-progress --omit=dev
+npm install --no-save --no-audit --no-fund --loglevel=error --no-progress --omit=dev --ignore-scripts
 npm run init
 
 echo "Entering SillyTavern..."
