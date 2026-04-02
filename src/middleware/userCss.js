0d7965789b0332f2e0232edf961dbb4003c3ca0b
Cohee <18619528+Cohee1207@users.noreply.github.com>
2026-04-02 19:45:36 +0300

    Update userCssMiddleware comment

--- src/middleware/userCss.js
+++ src/middleware/userCss.js
@@ -2,10 +2,7 @@ import path from 'node:path';
 import fs from 'node:fs';
 
 /**
- * Returns an Express middleware function that serves public override files from the data directory if they exist,
- * falling back to the default server public directory if not.
- * If the DATA_ROOT global variable is not defined or if there are issues with the public overrides configuration,
- * returns a no-op middleware.
+ * Provides an Express middleware function that serves a user-defined CSS file from the data directory if it exists.
  * @type {import('express').Handler}
  */
 export function userCssMiddleware(req, res, next) {
