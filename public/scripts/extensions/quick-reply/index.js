6c74367352e8b799c656a30401e30f6336c9665d
Wolfsblvt <wolfsblvt@gmail.com>
2026-04-09 21:46:23 +0200

    Convert jQuery/IIFE wrappers to exported init() functions and register activate hooks in 8 extension manifests

--- public/scripts/extensions/quick-reply/index.js
+++ public/scripts/extensions/quick-reply/index.js
@@ -214,7 +214,8 @@ const init = async () => {
     eventSource.on(event_types.APP_READY, async () => await finalizeInit());
 
     globalThis.quickReplyApi = quickReplyApi;
-};
+}
+
 const finalizeInit = async () => {
     debug('executing startup');
     await autoExec.handleStartup();
