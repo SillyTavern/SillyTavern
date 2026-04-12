8cc5eff45a1c36ad4fa6b96561eb9e38aade67c0
Reithan <bo122081@hotmail.com>
2026-04-11 23:30:13 -0700

    lint

--- public/scripts/textgen-settings.js
+++ public/scripts/textgen-settings.js
@@ -1756,7 +1756,7 @@ export function createTextGenGenerationData(settings, model, finalPrompt = null,
 
     if (settings.type === KOBOLDCPP) {
         params.grammar = settings.grammar_string || undefined;
-        params.grammar_retain_state = (isContinue ? true : false);
+        params.grammar_retain_state = (!!isContinue);
         params.trim_stop = true;
         params.dry_sequence_breakers = params.parseSequenceBreakers();
     }
