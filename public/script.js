9920096927d16fc088a7ac57a4efc78b2411afb0
Wolfsblvt <wolfsblvt@gmail.com>
2026-04-20 00:10:40 +0200

    fix: prevent event bubbling on show more messages button
    Replace mouseup/touchend with click event and add stopPropagation/preventDefault to prevent unintended event bubbling when loading more messages.

--- public/script.js
+++ public/script.js
@@ -12490,7 +12490,9 @@ jQuery(async function () {
         $('#avatar-and-name-block').slideToggle();
     });
 
-    $(document).on('mouseup touchend', '#show_more_messages', async function () {
+    $(document).on('click', '#show_more_messages', async function (event) {
+        event.stopPropagation();
+        event.preventDefault();
         await showMoreMessages();
     });
 
