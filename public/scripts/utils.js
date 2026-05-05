f02ee93c5cfcf0e0690aa18260baeaf711cc995c
Wolfsblvt <wolfsblvt@gmail.com>
2026-05-05 20:00:09 +0200

    feat: simplify getUniqueName by removing allowBaseName option and changing default startIndex to 0

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -692,18 +692,12 @@ export function isElementInViewport(el) {
  * @param {{ (name: string): boolean; }} exists Function to check if name exists.
  * @param {Object} [options] The options.
  * @param {((baseName: string, i: number) => string)|null} [options.nameBuilder=null] Function to build the name.
- *        Starts with the index provided by `startIndex` (default is 1). If not provided, uses "${baseName} (${i})".
+ *        Starts with the index provided by `startIndex` (default is 0). If not provided, uses `baseName` for index 0 and "${baseName} (${i})" for higher indices.
  * @param {number} [options.maxTries=1000] The maximum number of tries to find a unique name. Default is 1000.
- * @param {number} [options.startIndex=1] The index to start with when building the name. Default is 1.
- *        When set to 0, the intention is to also check if the basename (without applied index) is free.
- * @param {boolean} [options.allowBaseName=true] Whether to allow the base name to be returned if it's unique. Default is true.
+ * @param {number} [options.startIndex=0] The index to start with when building the name. Default is 0.
  * @returns {string|null} A unique name. Null if no unique name could be found in `maxTries`.
  */
-export function getUniqueName(baseName, exists, { nameBuilder = null, maxTries = 1000, startIndex = 1, allowBaseName = true } = {}) {
-    if (allowBaseName && !exists(baseName)) {
-        return baseName;
-    }
-
+export function getUniqueName(baseName, exists, { nameBuilder = null, maxTries = 1000, startIndex = 0 } = {}) {
     nameBuilder ??= (baseName, i) => i === 0 ? baseName : `${baseName} (${i})`;
     let i = startIndex;
     let name;
