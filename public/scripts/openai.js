e9307f4c0ef3e127157ea718f1993795059c83c7
Samueras <Samueras@jackoedv.de>
2026-06-15 08:35:55 +0200

    Add glm-5.2 to contextMap
    Add glm-5.2 to contextMap

--- public/scripts/openai.js
+++ public/scripts/openai.js
@@ -5152,6 +5152,7 @@ function getZaiMaxContext(model, isUnlocked) {
     }
 
     const contextMap = {
+        'glm-5.2': max_200k,
         'glm-5.1': max_200k,
         'glm-5-turbo': max_200k,
         'glm-5v-turbo': max_200k,
