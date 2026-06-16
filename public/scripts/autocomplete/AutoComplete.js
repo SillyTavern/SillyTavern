dbd50013c8a99e27c73963567cbe54e6866da6c5
Tommy <rsslone1120@gmail.com>
2026-06-16 01:19:46 -0400

    Fix AutoComplete dialogue on overflow
    This fixes cases where a text box has become full and has overflow. Scrolling the box did not place the autocomplete dialogue or macro tooltip properly.

--- public/scripts/autocomplete/AutoComplete.js
+++ public/scripts/autocomplete/AutoComplete.js
@@ -669,6 +669,7 @@ export class AutoComplete {
             mo.observe(this.textarea.parentElement, { childList: true });
         }
         this.clone.style.height = `${inputRect.height}px`;
+        this.clone.style.overflow = 'hidden';
         this.clone.style.left = `${inputRect.left}px`;
         this.clone.style.top = `${inputRect.top}px`;
         this.clone.style.whiteSpace = style.whiteSpace;
