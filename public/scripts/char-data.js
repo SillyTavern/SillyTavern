8901f28b0104ef2f8ca6b329c146b14ea2c78c99
Em <136832482+m98bw@users.noreply.github.com>
2026-09-24 07:16:59 +1000

    Fix typos in property descriptions in char-data.js (#6054)
    * Fix typos in property descriptions in char-data.js
    
    Wether → Whether x6
    
    * docs: correct character description property text
    
    ---------
    
    Co-authored-by: Wolfsblvt <wolfsblvt@gmail.com>

--- public/scripts/char-data.js
+++ public/scripts/char-data.js
@@ -33,12 +33,12 @@
  * @property {number} role - The specific function or purpose of the extension.
  * @property {boolean} vectorized - Indicates if the extension is optimized for vectorized processing.
  * @property {number} display_index - The order in which the extension should be displayed for user interfaces.
- * @property {boolean} match_persona_description - Wether to match against the persona description.
- * @property {boolean} match_character_description - Wether to match against the persona description.
- * @property {boolean} match_character_personality - Wether to match against the character personality.
- * @property {boolean} match_character_depth_prompt - Wether to match against the character depth prompt.
- * @property {boolean} match_scenario - Wether to match against the character scenario.
- * @property {boolean} match_creator_notes - Wether to match against the character creator notes.
+ * @property {boolean} match_persona_description - Whether to match against the persona description.
+ * @property {boolean} match_character_description - Whether to match against the character description.
+ * @property {boolean} match_character_personality - Whether to match against the character personality.
+ * @property {boolean} match_character_depth_prompt - Whether to match against the character depth prompt.
+ * @property {boolean} match_scenario - Whether to match against the character scenario.
+ * @property {boolean} match_creator_notes - Whether to match against the character creator notes.
  */
 
 /**
